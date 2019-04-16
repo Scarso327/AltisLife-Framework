1ee881d087b5852fa8192e450c65d65b21e33e65
lewis <lewisallanmackinnon@gmail.com>
2019-04-16 12:17:48 +0100

    Updated vehicle storage

--- Client Side/Framework/FF/Configuration/CfgVehicles.hpp
+++ Client Side/Framework/FF/Configuration/CfgVehicles.hpp
@@ -189,76 +189,62 @@ class LifeCfgVehicles {
     };
 
     class C_Quadbike_01_F { // Quad Bike
-        vItemSpace = 0;
+        vItemSpace = 25;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class O_Quadbike_01_F { // [Syndikat/Black Market] Quad Bike
-        vItemSpace = 0;
+        vItemSpace = 25;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_G_Quadbike_01_F { // [Chop Shop] Quad Bike
-        vItemSpace = 0;
+        vItemSpace = 25;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Hatchback_01_F { // Hatchback
-        vItemSpace = 0;
+        vItemSpace = 35;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Offroad_01_F { // Offroad
-        vItemSpace = 0;
+        vItemSpace = 65;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class O_G_Offroad_01_F { // [Syndikat/Black Market] Offroad
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class O_G_Offroad_01_armed_F { // [Syndikat/Black Market] Offroad (HMG)
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class C_Offroad_01_repair_F { // Offroad (Services)
-        vItemSpace = 0;
+    class O_G_Offroad_01_F { // [Syndikat] Offroad
+        vItemSpace = 65;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Offroad_02_unarmed_F { // MB 4WD
-        vItemSpace = 0;
+        vItemSpace = 60;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class I_C_Offroad_02_unarmed_F { // [Syndikat/Black Market] MB 4WD
-        vItemSpace = 0;
+    class I_C_Offroad_02_unarmed_F { // [Syndikat] MB 4WD
+        vItemSpace = 60;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class I_C_Offroad_02_LMG_F { // [Syndikat/Black Market] MB 4WD (LMG)
+    class I_C_Offroad_02_LMG_F { // [Syndika] MB 4WD (LMG)
         vItemSpace = 0;
         conditions = "";
         price = 0;
@@ -266,77 +252,49 @@ class LifeCfgVehicles {
     };
 
     class C_SUV_01_F { // SUV
-        vItemSpace = 0;
+        vItemSpace = 45;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Hatchback_01_sport_F { // Hatchback (Sport)
-        vItemSpace = 0;
+        vItemSpace = 35;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Van_02_transport_F { // Van Transport
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class B_LSV_01_unarmed_F { // Prowler (Unarmed)
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class O_LSV_02_unarmed_F { // Qilin (Unarmed)
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class B_MRAP_01_F { // Hunter
-        vItemSpace = 0;
+        vItemSpace = 90;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class O_G_Van_02_transport_F { // [Syndikat] Van Transport
-        vItemSpace = 0;
+        vItemSpace = 90;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Van_02_vehicle_F { // Van (Cargo)
-        vItemSpace = 0;
+        vItemSpace = 100;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class O_G_Van_02_vehicle_F { // [Syndikat] Van (Cargo)
-        vItemSpace = 0;
+        vItemSpace = 100;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Van_02_medevac_F { // Van (Ambulance)
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class C_Van_02_service_F { // Van (Services)
-        vItemSpace = 0;
+        vItemSpace = 70;
         conditions = "";
         price = 0;
         textures[] = {};
@@ -350,42 +308,28 @@ class LifeCfgVehicles {
     };
 
     class O_G_Van_01_transport_F { // [Syndikat] Truck
-        vItemSpace = 0;
+        vItemSpace = 125;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Van_01_box_F { // Truck Boxer
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class C_Van_01_fuel_F { // Fuel Truck
-        vItemSpace = 0;
+        vItemSpace = 150;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Truck_02_transport_F { // Zamak Transport
-        vItemSpace = 0;
+        vItemSpace = 190;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Truck_02_covered_F { // Zamak Transport (Covered)
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class C_Truck_02_fuel_F { // Zamak Fuel
-        vItemSpace = 0;
+        vItemSpace = 230;
         conditions = "";
         price = 0;
         textures[] = {};
@@ -399,154 +343,140 @@ class LifeCfgVehicles {
     };
 
     class B_Truck_01_transport_F { // HEMTT Transport
-        vItemSpace = 0;
+        vItemSpace = 250;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_Truck_01_covered_F { // HEMTT Transport (Covered)
-        vItemSpace = 0;
+        vItemSpace = 285;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_Truck_01_ammo_F { // HEMTT Ammo
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class B_Truck_01_fuel_F { // HEMTT Fuel
-        vItemSpace = 0;
+        vItemSpace = 300;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_Truck_01_box_F { // HEMTT Box
-        vItemSpace = 0;
+        vItemSpace = 750;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class O_Truck_03_transport_F { // Tempest Transport
-        vItemSpace = 0;
+    class O_T_Truck_03_transport_ghex_F { // Tempest Transport
+        vItemSpace = 370;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class O_Truck_03_covered_F { // Tempest Transport (Covered)
-        vItemSpace = 0;
+    class O_T_Truck_03_covered_ghex_F { // Tempest Transport (Covered)
+        vItemSpace = 395;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class O_Truck_03_ammo_F { // Tempest Ammo
-        vItemSpace = 0;
+    class O_T_Truck_03_ammo_ghex_F { // Tempest Ammo
+        vItemSpace = 480;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
-    class O_Truck_03_fuel_F { // Tempest Fuel
-        vItemSpace = 0;
-        conditions = "";
-        price = 0;
-        textures[] = {};
-    };
-
-    class O_Truck_03_device_F { // Tempest (Device)
-        vItemSpace = 0;
+    class O_T_Truck_03_device_ghex_F { // Tempest (Device)
+        vItemSpace = 500;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Heli_Light_01_civil_F { // M-900
-        vItemSpace = 0;
+        vItemSpace = 60;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_Heli_Light_01_F { // MH-9 Hummingbird
-        vItemSpace = 0;
+        vItemSpace = 75;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class O_Heli_Light_02_unarmed_F { // PO-30 Orca (Unarmed)
-        vItemSpace = 0;
+        vItemSpace = 190;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class I_Heli_light_03_unarmed_F { // WY-55 Hellcat (Unarmed)
-        vItemSpace = 0;
+        vItemSpace = 160;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class I_Heli_Transport_02_F { // CH-49 Mohawk
-        vItemSpace = 0;
+        vItemSpace = 245;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_Heli_Transport_03_unarmed_F { // CH-67 Huron (Unarmed)
-        vItemSpace = 0;
+        vItemSpace = 460;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Plane_Civil_01_F { // Caesar BTT
-        vItemSpace = 0;
+        vItemSpace = 40;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Scooter_Transport_01_F { // Water Scooter
-        vItemSpace = 0;
+        vItemSpace = 20;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Rubberboat { // Rescue Boat
-        vItemSpace = 0;
+        vItemSpace = 35;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Boat_Civil_01_F { // Motorboat
-        vItemSpace = 0;
+        vItemSpace = 50;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class C_Boat_Transport_02_F { // RHIB
-        vItemSpace = 0;
+        vItemSpace = 65;
         conditions = "";
         price = 0;
         textures[] = {};
     };
 
     class B_SDV_01_F { // SDV
-        vItemSpace = 0;
+        vItemSpace = 15;
         conditions = "";
         price = 0;
         textures[] = {};

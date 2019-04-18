71c57ffb3037cf798ac4bf998434c32458c8cdd2
Jack Farhall <jackfarhall@gmail.com>
2019-04-18 21:09:57 +0100

    Added: Police Rank Uniforms

--- Client Side/Framework/FF/Configuration/CfgTextures.hpp
+++ Client Side/Framework/FF/Configuration/CfgTextures.hpp
@@ -6,11 +6,29 @@
 
 class CfgTextures {
     class U_Rangemaster {
-        texture = "Police\Uniforms\Junior.paa";
+        texture[] = {
+            0,
+            "Police\Uniforms\Junior.paa"
+        };
         invisBackpack[] = {3, ""};
     };
     class U_Competitor {
-        texture = "Police\Uniforms\Senior.paa";
+        texture[] = {
+            1,
+            {
+                "Police\Uniforms\PC.paa", // Level 1
+                "Police\Uniforms\PC.paa", // Level 2
+                "Police\Uniforms\PC.paa", // Level 3
+                "Police\Uniforms\SGT.paa", // Level 4
+                "Police\Uniforms\INSP.paa", // Level 5
+                "Police\Uniforms\CINSP.paa", // Level 6
+                "Police\Uniforms\SUPT.paa", // Level 7
+                "Police\Uniforms\CSUPT.paa", // Level 8
+                "Police\Uniforms\DCOM.paa", // Level 9
+                "Police\Uniforms\COM.paa", // Level 10
+                "Police\Uniforms\CMDR.paa" // Level 11
+            }
+        };
         invisBackpack[] = {3, ""};
     };
 };

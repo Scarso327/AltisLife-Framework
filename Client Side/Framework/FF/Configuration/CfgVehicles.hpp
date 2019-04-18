d0863f7579226b9704e9c1a0fbb5d742ce5d2997
Jack Farhall <jackfarhall@gmail.com>
2019-04-18 22:18:46 +0100

    Added: Bunch of Vehicle Textures (Default + 2 Police)

--- Client Side/Framework/FF/Configuration/CfgVehicles.hpp
+++ Client Side/Framework/FF/Configuration/CfgVehicles.hpp
@@ -192,35 +192,115 @@ class LifeCfgVehicles {
         vItemSpace = 25;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Black", "civ", {
+                "\A3\Soft_F_beta\Quadbike_01\Data\quadbike_01_civ_black_co.paa"
+            }, "" },
+            { "Blue", "civ", {
+                "\A3\Soft_F_beta\Quadbike_01\Data\quadbike_01_civ_blue_co.paa"
+            }, "" },
+            { "Red", "civ", {
+                "\A3\Soft_F_beta\Quadbike_01\Data\quadbike_01_civ_red_co.paa"
+            }, "" },
+            { "White", "civ", {
+                "\A3\Soft_F_beta\Quadbike_01\Data\quadbike_01_civ_white_co.paa"
+            }, "" },
+            { "Digi Green", "civ", {
+                "\A3\Soft_F_beta\Quadbike_01\Data\quadbike_01_indp_co.paa"
+            }, "" },
+            { "Hunter Camo", "civ", {
+                "\a3\soft_f_gamma\Quadbike_01\data\quadbike_01_indp_hunter_co.paa"
+            }, "" }
+        };
     };
 
     class O_Quadbike_01_F { // [Syndikat/Black Market] Quad Bike
         vItemSpace = 25;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Hunter Camo", "civ", {
+                "\a3\soft_f_gamma\Quadbike_01\data\quadbike_01_indp_hunter_co.paa"
+            }, "" }
+        };
     };
 
     class B_G_Quadbike_01_F { // [Chop Shop] Quad Bike
         vItemSpace = 25;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Hunter Camo", "civ", {
+                "\a3\soft_f_gamma\Quadbike_01\data\quadbike_01_indp_hunter_co.paa"
+            }, "" }
+        };
     };
 
     class C_Hatchback_01_F { // Hatchback
         vItemSpace = 35;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Beige", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base01_co.paa"
+            }, "" },
+            { "Green", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base02_co.paa"
+            }, "" },
+            { "Blue", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base03_co.paa"
+            }, "" },
+            { "Dark Blue", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base04_co.paa"
+            }, "" },
+            { "Yellow", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base06_co.paa"
+            }, "" },
+            { "White", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base07_co.paa"
+            }, "" },
+            { "Grey", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base08_co.paa"
+            }, "" },
+            { "Black", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_base09_co.paa"
+            }, "" }
+        };
     };
 
     class C_Offroad_01_F { // Offroad
         vItemSpace = 65;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Red", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_co.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_co.paa"
+            }, "" },
+            { "Yellow", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE01_CO.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE01_CO.paa"
+            }, "" },
+            { "White", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE02_CO.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE02_CO.paa"
+            }, "" },
+            { "Blue", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE03_CO.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE03_CO.paa"
+            }, "" },
+            { "Dark Red", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE04_CO.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE04_CO.paa"
+            }, "" },
+            { "Blue / White", "civ", {
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE05_CO.paa",
+                "\A3\soft_F\Offroad_01\Data\offroad_01_ext_BASE05_CO.paa"
+            }, "" },
+            { "Police", "cop", {
+                 "data\textures\Police\Cars\Police_offroad.paa"
+            }, "" }
+        };
     };
 
     class O_G_Offroad_01_F { // [Syndikat] Offroad
@@ -241,7 +321,26 @@ class LifeCfgVehicles {
         vItemSpace = 60;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Black", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_black_co.paa"
+            }, "" },
+            { "Blue", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_blue_co.paa"
+            }, "" },
+            { "Green", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_green_co.paa"
+            }, "" },
+            { "Orange", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_orange_co.paa"
+            }, "" },
+            { "Red", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_red_co.paa"
+            }, "" },
+            { "White", "civ", {
+                "\A3\Soft_F_Exp\Offroad_02\Data\offroad_02_ext_white_co.paa"
+            }, "" }
+        };
     };
 
     class I_C_Offroad_02_LMG_F { // [Syndika] MB 4WD (LMG)
@@ -255,14 +354,49 @@ class LifeCfgVehicles {
         vItemSpace = 45;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Dark Red", "civ", {
+                "\a3\soft_f_gamma\SUV_01\Data\suv_01_ext_co.paa"
+            }, "" },
+            { "Silver", "civ", {
+                "\a3\soft_f_gamma\SUV_01\Data\suv_01_ext_03_co.paa"
+            }, "" },
+            { "Orange", "civ", {
+                "\a3\soft_f_gamma\SUV_01\Data\suv_01_ext_04_co.paa"
+            }, "" },
+            { "Medic", "med", {
+                 "textures\medic_cars\Medic_SUV.paa"
+            }, "" },
+            { "Police", "cop", {
+                "data\textures\Police\Cars\Police_SUV_White.paa"
+            }, "" }
+        };
     };
 
     class C_Hatchback_01_sport_F { // Hatchback (Sport)
         vItemSpace = 35;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Red", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport01_co.paa"
+            }, "" },
+            { "Dark Blue", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport02_co.paa"
+            }, "" },
+            { "Orange", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport03_co.paa"
+            }, "" },
+            { "Black / White", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport04_co.paa"
+            }, "" },
+            { "Beige", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport05_co.paa"
+            }, "" },
+            { "Green", "civ", {
+                "\a3\soft_f_gamma\Hatchback_01\data\hatchback_01_ext_sport06_co.paa"
+            }, "" }
+        };
     };
 
     class C_Van_02_transport_F { // Van Transport
@@ -409,14 +543,52 @@ class LifeCfgVehicles {
         vItemSpace = 75;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Sheriff", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sheriff_co.paa"
+            }, "" },
+            { "Civ Blue", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_blue_co.paa"
+            }, "" },
+            { "Civ Red", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_co.paa"
+            }, "" },
+            { "Blueline", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_blueline_co.paa"
+            }, "" },
+            { "Elliptical", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_elliptical_co.paa"
+            }, "" },
+            { "Furious", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_furious_co.paa"
+            }, "" },
+            { "Jeans Blue", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_jeans_co.paa"
+            }, "" },
+            { "Speedy Redline", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_speedy_co.paa"
+            }, "" },
+            { "Sunset", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sunset_co.paa"
+            }, "" },
+            { "Vrana", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_vrana_co.paa"
+            }, "" },
+            { "Waves Blue", "civ", {
+                "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_wave_co.paa"
+            }, "" }
+        };
     };
 
     class O_Heli_Light_02_unarmed_F { // PO-30 Orca (Unarmed)
         vItemSpace = 190;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "White / Blue", "civ", {
+                "\a3\air_f\Heli_Light_02\Data\heli_light_02_ext_civilian_co.paa"
+            }, "" }
+        };
     };
 
     class I_Heli_light_03_unarmed_F { // WY-55 Hellcat (Unarmed)
@@ -444,14 +616,92 @@ class LifeCfgVehicles {
         vItemSpace = 40;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Racing (Tan Interior)", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Racer_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Racer_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_tan_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_tan_co.paa"
+            }, "" },
+            { "Racing", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Racer_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Racer_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_co.paa"
+            }, "" },
+            { "Red Line (Tan Interior)", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_RedLine_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_RedLine_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_tan_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_tan_co.paa"
+            }, "" },
+            { "Red Line", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_RedLine_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_RedLine_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_co.paa"
+            }, "" },
+            { "Tribal (Tan Interior)", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Tribal_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Tribal_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_tan_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_tan_co.paa"
+            }, "" },
+            { "Tribal", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Tribal_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Tribal_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_co.paa"
+            }, "" },
+            { "Blue Wave (Tan Interior)", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Wave_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Wave_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_tan_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_tan_co.paa"
+            }, "" },
+            { "Blue Wave", "civ", {
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Wave_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Wave_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_01_co.paa",
+                "A3\Air_F_Exp\Plane_Civil_01\Data\btt_int_02_co.paa"
+            }, "" }
+        };    
     };
 
     class C_Scooter_Transport_01_F { // Water Scooter
         vItemSpace = 20;
         conditions = "";
         price = 0;
-        textures[] = {};
+        textures[] = {
+            { "Black", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Black_CO.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_Black_CO.paa"
+            }, "" },
+            { "Blue", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Blue_co.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_Blue_co.paa"
+            }, "" },
+            { "Grey", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Grey_co.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_Grey_co.paa"
+            }, "" },
+            { "Green", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Lime_co.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_Lime_co.paa"
+            }, "" },
+            { "Red", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Red_CO.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_CO.paa"
+            }, "" },
+            { "White", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_CO.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_CO.paa"
+            }, "" },
+            { "Yellow", "civ", {
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_Yellow_CO.paa",
+                "\A3\Boat_F_Exp\Scooter_Transport_01\Data\Scooter_Transport_01_VP_Yellow_CO.paa"
+            }, "" }
+        };
     };
 
     class C_Rubberboat { // Rescue Boat

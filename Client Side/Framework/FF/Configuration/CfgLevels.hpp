07f640660a25b2e4f1117a6c1daf6d9803958d4a
lewis <lewisallanmackinnon@gmail.com>
2019-04-19 03:50:31 +0100

    Updated all levels

--- Client Side/Framework/FF/Configuration/CfgLevels.hpp
+++ Client Side/Framework/FF/Configuration/CfgLevels.hpp
@@ -1,42 +1,215 @@
 class CfgLevels {
 
     maxLevel = 5;
-    maxLevelLoopReward = 100000; // £100,000...
+    maxLevelLoopReward = 100000;
 
     class level_1 {
         displayNameLong = "Level 1";
         displayNameShort = "1";
         expRequired = 0;
-        perkUnlockeds[] = {
-            "seatbelt"
-        };
+        perkUnlockeds[] = {};
     };
     
     class level_2 {
         displayNameLong = "Level 2";
         displayNameShort = "2";
-        expRequired = 200;
+        expRequired = 100;
         perkUnlockeds[] = {};
     };
     
     class level_3 {
         displayNameLong = "Level 3";
         displayNameShort = "3";
-        expRequired = 1000;
-        perkUnlockeds[] = {};
+        expRequired = 250;
+        perkUnlockeds[] = {"muscleman"};
     };
     
     class level_4 {
         displayNameLong = "Level 4";
         displayNameShort = "4";
-        expRequired = 1700;
+        expRequired = 500;
         perkUnlockeds[] = {};
     };
     
     class level_5 {
         displayNameLong = "Level 5";
         displayNameShort = "5";
-        expRequired = 2300;
+        expRequired = 750;
         perkUnlockeds[] = {};
+    };
+
+    class level_6 {
+        displayNameLong = "Level 6";
+        displayNameShort = "6";
+        expRequired = 1000;
+        perkUnlockeds[] = {"pettyThief"};
+    };
+    
+    class level_7 {
+        displayNameLong = "Level 7";
+        displayNameShort = "7";
+        expRequired = 1500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_8 {
+        displayNameLong = "Level 8";
+        displayNameShort = "8";
+        expRequired = 2000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_9 {
+        displayNameLong = "Level 9";
+        displayNameShort = "9";
+        expRequired = 2500;
+        perkUnlockeds[] = {"firstAider"};
+    };
+    
+    class level_10 {
+        displayNameLong = "Level 10";
+        displayNameShort = "10";
+        expRequired = 3000;
+        perkUnlockeds[] = {};
+    };
+
+    class level_11 {
+        displayNameLong = "Level 11";
+        displayNameShort = "11";
+        expRequired = 3500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_12 {
+        displayNameLong = "Level 12";
+        displayNameShort = "12";
+        expRequired = 4000;
+        perkUnlockeds[] = {"sweetTalker"};
+    };
+    
+    class level_13 {
+        displayNameLong = "Level 13";
+        displayNameShort = "13";
+        expRequired = 4500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_14 {
+        displayNameLong = "Level 14";
+        displayNameShort = "14";
+        expRequired = 5000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_15 {
+        displayNameLong = "Level 15";
+        displayNameShort = "15";
+        expRequired = 5500;
+        perkUnlockeds[] = {"generalMechanic"};
+    };
+
+    class level_16 {
+        displayNameLong = "Level 16";
+        displayNameShort = "6";
+        expRequired = 6000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_17 {
+        displayNameLong = "Level 17";
+        displayNameShort = "17";
+        expRequired = 6500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_18 {
+        displayNameLong = "Level 18";
+        displayNameShort = "18";
+        expRequired = 7000;
+        perkUnlockeds[] = {"expertFinancer"};
+    };
+    
+    class level_19 {
+        displayNameLong = "Level 19";
+        displayNameShort = "19";
+        expRequired = 7500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_20 {
+        displayNameLong = "Level 20";
+        displayNameShort = "20";
+        expRequired = 10000;
+        perkUnlockeds[] = {};
+    };
+
+    class level_21 {
+        displayNameLong = "Level 11";
+        displayNameShort = "21";
+        expRequired = 10000;
+        perkUnlockeds[] = {"wideTraveller"};
+    };
+    
+    class level_22 {
+        displayNameLong = "Level 22";
+        displayNameShort = "22";
+        expRequired = 10000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_23 {
+        displayNameLong = "Level 23";
+        displayNameShort = "23";
+        expRequired = 10000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_24 {
+        displayNameLong = "Level 24";
+        displayNameShort = "24";
+        expRequired = 10500;
+        perkUnlockeds[] = {"experiencedProcessor"};
+    };
+    
+    class level_25 {
+        displayNameLong = "Level 25";
+        displayNameShort = "25";
+        expRequired = 11000;
+        perkUnlockeds[] = {};
+    };
+
+    class level_26 {
+        displayNameLong = "Level 26";
+        displayNameShort = "6";
+        expRequired = 11500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_27 {
+        displayNameLong = "Level 27";
+        displayNameShort = "17";
+        expRequired = 12000;
+        perkUnlockeds[] = {"hardWorker"};
+    };
+    
+    class level_28 {
+        displayNameLong = "Level 28";
+        displayNameShort = "28";
+        expRequired = 12500;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_29 {
+        displayNameLong = "Level 29";
+        displayNameShort = "29";
+        expRequired = 13000;
+        perkUnlockeds[] = {};
+    };
+    
+    class level_30 {
+        displayNameLong = "Level 30";
+        displayNameShort = "30";
+        expRequired = 20000;
+        perkUnlockeds[] = {"highRoller"};
     };
 };

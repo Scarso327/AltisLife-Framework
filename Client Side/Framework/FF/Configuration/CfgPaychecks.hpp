a21def03d5f419f77898ad678272f4f5f4666912
Jack Farhall <jackfarhall@gmail.com>
2019-04-19 19:36:50 +0100

    Same as last

--- Client Side/Framework/FF/Configuration/CfgPaychecks.hpp
+++ Client Side/Framework/FF/Configuration/CfgPaychecks.hpp
@@ -8,15 +8,23 @@
 
 class CfgPaychecks {
     class west {
-		class life_coplevel {
-			paychecks[] = {
-				1000, // Level 0
-				1000, // Level 1
-				1000, // Level 2
-				1000, // Level 3
-				1000, // Level 4
-				1000, // Level 5
-				1000 // Level 6
+		class levels {
+			class life_coplevel {
+				paychecks[] = {
+					1000, // Level 0
+					1000, // Level 1
+					1000, // Level 2
+					1000, // Level 3
+					1000, // Level 4
+					1000, // Level 5
+					1000 // Level 6
+				};
+			};
+		};
+
+		class licenses {
+			class aso3 {
+				paycheck = 1000;
 			};
 		};
 	};

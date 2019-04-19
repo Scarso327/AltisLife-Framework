eef7602d26b6502678f029c4d7296764477b28d3
lewis <lewisallanmackinnon@gmail.com>
2019-04-19 03:32:10 +0100

    Updated perks

--- Client Side/Framework/FF/Configuration/CfgPerks.hpp
+++ Client Side/Framework/FF/Configuration/CfgPerks.hpp
@@ -1,14 +1,67 @@
 class CfgPerks {
-
-    class seatbelt {
-        displayName = "Auto-Seatbelt";
-        description = "Automatically take and put your seatbelt on/off when entering/exiting a vehicle.";
+    class expertFinancer {
+        displayName = "Expert Financer";
+        description = "Balance your finances like an expert and earn your 10% extra from your paychecks!";
         sides[] = {};
     };
 
-    class med_lucky {
-        displayName = "Medical Lottery";
-        description = "Increased chances of the defib working first try.";
-        sides[] = { "med" };
+    class generalMechanic {
+        displayName = "General Mechanic";
+        description = "Take a machanic's course and learn how to reapir your vehicle without breaking your toolkits!";
+        sides[] = {};
+    };
+
+    class firstAider {
+        displayName = "First Aider";
+        description = "Ration your equipment to extend your first aid kits uses from 1 to 3!";
+        sides[] = {};
+    };
+
+    class wideTraveller {
+        displayName = "Wide Traveller";
+        description = "Get around and meet people! Gain the ability to spawn in more areas across the map.";
+        sides[] = {"civ"};
+    };
+
+    class muscleman {
+        displayName = "Muscleman";
+        description = "Get a gym membership and build your muscles to the point where you can carry 30% more items!";
+        sides[] = {};
+    };
+
+    class pettyThief {
+        displayName = "Petty Thief";
+        description = "Become a great locksmith and increase your chances of scuess when lockpicking or boltcutting by 25%.";
+        sides[] = {"civ"};
+    };
+
+    class highRoller {
+        displayName = "High Roller";
+        description = "You win some you lose some... But you've learned how to win and win again with your 20% increased chances as the casino.";
+        sides[] = {};
+    };
+
+    class sweetTalker {
+        displayName = "Sweet Talker";
+        description = "Learn how to talk-the-talk and increase the worth of all your sales by 10%.";
+        sides[] = {};
+    };
+
+    class experiencedProcessor {
+        displayName = "Experienced Processor";
+        description = "Working the materials of Tanoa can be difficult - But not for you. Increase your processing speed by 50%.";
+        sides[] = {"civ"};
+    };
+
+    class fisherman {
+        displayName = "Fisherman";
+        description = "Become one with the sea and catch masses amounts of fish with your 50% increased chances of catching a fish.";
+        sides[] = {"civ"};
+    };
+
+    class hardWorker {
+        displayName = "Hard Worker";
+        description = "Pay no storage fees from the garages across Tanoa as a result of all your hard work.";
+        sides[] = {"civ"};
     };
 };

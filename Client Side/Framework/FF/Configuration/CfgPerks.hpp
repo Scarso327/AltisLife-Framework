62166f5ec26bca98c9b2da3a3282725c398b3130
Lewis Mackinnon <lewisallanmackinnon@gmail.com>
2019-05-04 18:43:34 +0100

    Updated perks.

--- Client Side/Framework/FF/Configuration/CfgPerks.hpp
+++ Client Side/Framework/FF/Configuration/CfgPerks.hpp
@@ -1,28 +1,14 @@
-class CfgPerks {
-    class fisherman {
-        displayName = "Fisherman";
-        description = "Become one with the sea and catch masses amounts of fish with your <t color = '#7300e6'>50%</t> increased chances of catching a fish.";
-        sides[] = {"Civilian"};
-        unlockLevel = 1;
-    };
-    
-    class expertAider {
-        displayName = "Expert Aider";
-        description = "Increase the speed on which you provide aid to those injured by <t color = '#7300e6'>25%</t>.";
-        sides[] = {"Medic"};
-        unlockLevel = 1;
-    };
-
+class CfgPerks { 
     class muscleman {
         displayName = "Muscleman";
         description = "Get a gym membership and build your muscles to the point where you can carry <t color = '#7300e6'>30%</t> more items!";
         sides[] = {};
-        unlockLevel = 2;
+        unlockLevel = 3;
     };
 
-    class quickLearner {
-        displayName = "Quick Learner";
-        description = "Learn to master your pofessions at a much quicker rate!";
+    class fisherman {
+        displayName = "Fisherman";
+        description = "Become one with the sea and catch masses amounts of fish with your <t color = '#7300e6'>50%</t> increased chances of catching a fish.";
         sides[] = {"Civilian"};
         unlockLevel = 3;
     };
@@ -31,91 +17,105 @@ class CfgPerks {
         displayName = "Dedicated Athlete";
         description = "Train and push yourself as far as you possibly can in order to gain <t color = '#7300e6'>30%</t> less fatigue over time.";
         sides[] = {""};
-        unlockLevel = 4;
+        unlockLevel = 6;
     };
 
     class mindfulPlanner {
         displayName = "Mindful Planner";
         description = "Plan your journey fully and spawn with a <t color = '#7300e6'>GPS</t> and <t color = '#7300e6'>NV Goggles</t>.";
         sides[] = {"Civilian"};
-        unlockLevel = 4;
+        unlockLevel = 6;
     };
 
     class ecoDriver {
         displayName = "Eco Driver";
         description = "Become an ecological driver and, as a result, spend <t color = '#7300e6'>75%</t> less on fuel at fuel stations.";
         sides[] = {};
-        unlockLevel = 4;
+        unlockLevel = 9;
     };
 
     class pettyThief {
         displayName = "Petty Thief";
         description = "Become a great locksmith and increase your chances of scuess when lockpicking or boltcutting by <t color = '#7300e6'>25%</t>.";
         sides[] = {"Civilian"};
-        unlockLevel = 6;
-    };
-
-    class firstAider {
-        displayName = "First Aider";
-        description = "Ration your equipment to extend your first aid kits uses from 1 to 3!";
-        sides[] = {};
         unlockLevel = 9;
-    };
-
-    class sweetTalker {
-        displayName = "Sweet Talker";
-        description = "Learn how to talk-the-talk and increase the worth of all your sales by <t color = '#7300e6'>10%</t>.";
-        sides[] = {};
-        unlockLevel = 12;
     };
 
     class generalMechanic {
         displayName = "General Mechanic";
         description = "Take a machanic's course and learn how to reapir your vehicle without breaking your toolkits!";
         sides[] = {};
-        unlockLevel = 15;
+        unlockLevel = 12;
+    };
+
+    class quickLearner {
+        displayName = "Quick Learner";
+        description = "Learn to master your pofessions at a much quicker rate!";
+        sides[] = {"Civilian"};
+        unlockLevel = 12;
     };
 
     class expertFinancer {
         displayName = "Expert Financer";
         description = "Balance your finances like an expert and earn your <t color = '#7300e6'>10%</t> extra from your paychecks!";
         sides[] = {};
-        unlockLevel = 18;
+        unlockLevel = 15;
     };
 
     class wideTraveller {
         displayName = "Wide Traveller";
         description = "Get around and meet people! Gain the ability to spawn in more areas across the map.";
         sides[] = {"Civilian"};
-        unlockLevel = 21;
+        unlockLevel = 15;
     };
 
-    class experiencedProcessor {
-        displayName = "Experienced Processor";
-        description = "Working the materials of Tanoa can be difficult - But not for you. Increase your processing speed by <t color = '#7300e6'>50%</t>.";
-        sides[] = {"Civilian"};
-        unlockLevel = 24;
+    class sweetTalker {
+        displayName = "Sweet Talker";
+        description = "Learn how to talk-the-talk and increase the worth of all your sales by <t color = '#7300e6'>10%</t>.";
+        sides[] = {};
+        unlockLevel = 18;
     };
 
     class hardWorker {
         displayName = "Hard Worker";
         description = "Pay no storage fees from the garages across Tanoa as a result of all your hard work.";
         sides[] = {"Civilian"};
-        unlockLevel = 27;
+        unlockLevel = 18;
+    };
+
+    class improvedMetabolism {
+        displayName = "Improved Metabolism";
+        description = "Improve your metabolism and, as a result, your hunger and thrist levels go down <t color = '#7300e6'>30%</t> slower.";
+        sides[] = {""};
+        unlockLevel = 21;
     };
 
     class uncertainPersonality {
         displayName = "Uncertain Personality";
         description = "When chop shopping vehicles, you will have a <t color = '#7300e6'>10%</t> of being able to cancel the process and add the vehicle to your garage - obviously with some cost.";
         sides[] = {};
-        unlockLevel = 28;
+        unlockLevel = 21;
+    };
+
+    class secondNature {
+        displayName = "Second Nature";
+        description = "When entering a vehicle, you aquire the second nature to automatically put on your <t color = '#7300e6'>seatbelt</t> and <t color = '#7300e6'>lock</t> your car.";
+        sides[] = {};
+        unlockLevel = 24;
     };
 
     class propertyBroker {
         displayName = "Property Broker";
         description = "Master the art of being an estate agent and understand that the prices you see, may not be the price you pay. Reduce all property costs by <t color = '#7300e6'>15%</t>.";
         sides[] = {"Civilian"};
-        unlockLevel = 29;
+        unlockLevel = 24;
+    };
+
+    class slightOfHand {
+        displayName = "Slight of Hand";
+        description = "Understand your weapon and the ammo that it uses to <t color = '#7300e6'>reback</t> all your bullets, using lesser, but fuller, mags.";
+        sides[] = {""};
+        unlockLevel = 27;
     };
 
     class highRoller {

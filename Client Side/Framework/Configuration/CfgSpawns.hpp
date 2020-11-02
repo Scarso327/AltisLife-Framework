3d5836649aa94ce3444ee0d7031adbf4e34518e4
Lewis Mackinnon <lewisallanmackinnon@gmail.com>
2020-11-02 11:36:27 +0000

    Updated spawn points

--- Client Side/Framework/Configuration/CfgSpawns.hpp
+++ Client Side/Framework/Configuration/CfgSpawns.hpp
@@ -1,44 +1,147 @@
-/*
-*    Format:
-*        3: STRING (Conditions) - Must return boolean :
-*            String can contain any amount of conditions, aslong as the entire
-*            string returns a boolean. This allows you to check any levels, licenses etc,
-*            in any combination. For example:
-*                "[""Police_Main"", 1] call ULP_fnc_hasAccess;"
-*            This will also let you call any other function.
-*
-*/
 class CfgSpawns {
-
     class Altis {
         class Police {};
         class Medic {};
         class Civilian {};
     };
 
-    class Tanoa {
-        class Police {
+     class Tanoa {
+        class Civilian {
+            // Default Civilian Spawn Points
+            class civ_eorgetown {
+                displayName = "Georgetown";
+                spawnMarker = "civ_spawn_1";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            class civ_la_rochelle {
+                displayName = "La Rochelle";
+                spawnMarker = "civ_spawn_2";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            class civ_lijnhaven {
+                displayName = "Lijnhaven";
+                spawnMarker = "civ_spawn_3";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            // 'Wide Traveller' Spawn Points
+            class civ_nicolet {
+                displayName = "Nicolet";
+                spawnMarker = "civ_spawn_4";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_oumere {
+                displayName = "Ouméré";
+                spawnMarker = "civ_spawn_5";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_moddergat {
+                displayName = "Moddergat";
+                spawnMarker = "civ_spawn_6";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_katkoula {
+                displayName = "Katkoula";
+                spawnMarker = "civ_spawn_7";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_tuvanaka {
+                displayName = "Tuvanaka";
+                spawnMarker = "civ_spawn_8";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            // Job Specific Spawn Points
+            class civ_idap {
+                displayName = "IDAP Camp";
+                spawnMarker = "civ_spawn_idap";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "license_civ_idap";
+            };
+
+            class civ_aan {
+                displayName = "AAN World News";
+                spawnMarker = "civ_spawn_aan";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "license_civ_aan";
+            };
+        };
+
+        class Cop {
+            // Default TPC Spawn Points
             class tpc_georgetown {
                 displayName = "Georgetown HQ";
                 spawnMarker = "cop_spawn_1";
                 icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
                 conditions = "";
             };
+
+            class tpc_la_rochelle {
+                displayName = "La Rochelle HQ";
+                spawnMarker = "cop_spawn_2";
+                icon = "\a3\ui_f\data\map\MapControl\lighthouse_CA.paa";
+                conditions = "";
+            };
+
+            class tpc_lijnhaven {
+                displayName = "Lijnhaven HQ";
+                spawnMarker = "cop_spawn_3";
+                icon = "\a3\ui_f\data\map\MapControl\fountain_CA.paa";
+                conditions = "";
+            };
+
+            // Specilised TPC Spawn Points
+            class tpc_docks {
+                displayName = "Southern Docks";
+                spawnMarker = "cop_spawn_4";
+                icon = "\a3\ui_f\data\map\Markers\NATO\c_ship.paa";
+                conditions = "[""Police_ASO"", 1] call ULP_fnc_hasAccess || [""Police_SFO"", 1] call ULP_fnc_hasAccess";
+            };
+
+            class tpc_combinded {
+                displayName = "Joint Marine HQ";
+                spawnMarker = "cop_spawn_5";
+                icon = "\a3\ui_f\data\map\Markers\NATO\c_ship.paa";
+                conditions = "[""Police_ASO"", 1] call ULP_fnc_hasAccess || [""Police_SFO"", 1] call ULP_fnc_hasAccess";
+            };
         };
+
         class Medic {
+            // Default TAS Spawn Points
             class tas_georgetown {
                 displayName = "Georgetown Clinic";
                 spawnMarker = "medic_spawn_1";
                 icon = "\a3\ui_f\data\map\VehicleIcons\pictureLogic_ca.paa";
                 conditions = "";
             };
-        };
-        class Civilian {
-            class civ_eorgetown {
-                displayName = "Georgetown";
-                spawnMarker = "civ_spawn_1";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+
+            class tas_combinded {
+                displayName = "Joint Marine HQ";
+                spawnMarker = "medic_spawn_2";
+                icon = "\a3\ui_f\data\map\Markers\NATO\c_ship.paa";
                 conditions = "";
+            };
+
+            // SPecilised TAS Spawn Points
+            class tas_airbase {
+                displayName = "Southen Airbase";
+                spawnMarker = "medic_spawn_3";
+                icon = "\a3\ui_f\data\map\Markers\NATO\c_air.paa";
+                conditions = "[""Medic_TAA"", 1] call ULP_fnc_hasAccess";
             };
         };
     };

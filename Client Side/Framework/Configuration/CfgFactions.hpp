d245f06dac6eebddfb487922f6942d1b040730fa
Lewis Mackinnon <lewisallanmackinnon@gmail.com>
2020-11-02 19:56:56 +0000

    Fixed whitespace

--- Client Side/Framework/Configuration/CfgFactions.hpp
+++ Client Side/Framework/Configuration/CfgFactions.hpp
@@ -11,19 +11,23 @@ class CfgFactions {
 				payMulti = 0.1; // Pay increase per level...
 				required = 1;
 			};
+
 			class ASO {
 				queryIndex = 13;
 			};
+
 			class SFO {
 				queryIndex = 14;
 			};
 		};
+
 		class DatabaseInfo {
 			queryPrefix = "cop_";
 			customQuery = ", cop_blacklisted, coplevel, asolevel, sfolevel";
 			timeIndex = 0;
 		};
 	};
+
 	class Medic {
 		displayName = "Tanoa Ambulance Service";
 		pay = 2500;
@@ -31,16 +35,19 @@ class CfgFactions {
 			class Main {
 				queryIndex = 12;
 			};
+			
 			class TAA {
 				queryIndex = 13;
 			};
 		};
+
 		class DatabaseInfo {
 			queryPrefix = "med_";
 			customQuery = ", med_blacklisted, mediclevel, taalevel";
 			timeIndex = 1;
 		};
 	};
+
 	class Civilian {
 		displayName = "Civilian Population";
 		pay = 2500;

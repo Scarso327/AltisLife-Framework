cd213a8a03034ddd272cce14af8482ecb7f9b911
Jack Farhall <jackfarhall@gmail.com>
2020-11-02 11:51:22 +0000

    Fixed : CfgSpawns

--- Client Side/Framework/Configuration/CfgSpawns.hpp
+++ Client Side/Framework/Configuration/CfgSpawns.hpp
@@ -6,82 +6,7 @@ class CfgSpawns {
     };
 
      class Tanoa {
-        class Civilian {
-            // Default Civilian Spawn Points
-            class civ_eorgetown {
-                displayName = "Georgetown";
-                spawnMarker = "civ_spawn_1";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "";
-            };
-
-            class civ_la_rochelle {
-                displayName = "La Rochelle";
-                spawnMarker = "civ_spawn_2";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "";
-            };
-
-            class civ_lijnhaven {
-                displayName = "Lijnhaven";
-                spawnMarker = "civ_spawn_3";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "";
-            };
-
-            // 'Wide Traveller' Spawn Points
-            class civ_nicolet {
-                displayName = "Nicolet";
-                spawnMarker = "civ_spawn_4";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
-            };
-
-            class civ_oumere {
-                displayName = "Ouméré";
-                spawnMarker = "civ_spawn_5";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
-            };
-
-            class civ_moddergat {
-                displayName = "Moddergat";
-                spawnMarker = "civ_spawn_6";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
-            };
-
-            class civ_katkoula {
-                displayName = "Katkoula";
-                spawnMarker = "civ_spawn_7";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
-            };
-
-            class civ_tuvanaka {
-                displayName = "Tuvanaka";
-                spawnMarker = "civ_spawn_8";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
-            };
-
-            // Job Specific Spawn Points
-            class civ_idap {
-                displayName = "IDAP Camp";
-                spawnMarker = "civ_spawn_idap";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "license_civ_idap";
-            };
-
-            class civ_aan {
-                displayName = "AAN World News";
-                spawnMarker = "civ_spawn_aan";
-                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
-                conditions = "license_civ_aan";
-            };
-        };
-
-        class Cop {
+        class Police {
             // Default TPC Spawn Points
             class tpc_georgetown {
                 displayName = "Georgetown HQ";
@@ -143,6 +68,81 @@ class CfgSpawns {
                 icon = "\a3\ui_f\data\map\Markers\NATO\c_air.paa";
                 conditions = "[""Medic_TAA"", 1] call ULP_fnc_hasAccess";
             };
+        };
+
+        class Civilian {
+            // Default Civilian Spawn Points
+            class civ_eorgetown {
+                displayName = "Georgetown";
+                spawnMarker = "civ_spawn_1";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            class civ_la_rochelle {
+                displayName = "La Rochelle";
+                spawnMarker = "civ_spawn_2";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            class civ_lijnhaven {
+                displayName = "Lijnhaven";
+                spawnMarker = "civ_spawn_3";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "";
+            };
+
+            // 'Wide Traveller' Spawn Points
+            /*class civ_nicolet {
+                displayName = "Nicolet";
+                spawnMarker = "civ_spawn_4";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_oumere {
+                displayName = "Ouméré";
+                spawnMarker = "civ_spawn_5";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_moddergat {
+                displayName = "Moddergat";
+                spawnMarker = "civ_spawn_6";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_katkoula {
+                displayName = "Katkoula";
+                spawnMarker = "civ_spawn_7";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            class civ_tuvanaka {
+                displayName = "Tuvanaka";
+                spawnMarker = "civ_spawn_8";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "(FF_Perks findIf { _x isEqualTo ""wideTraveller"" }) > -1";
+            };
+
+            // Job Specific Spawn Points
+            class civ_idap {
+                displayName = "IDAP Camp";
+                spawnMarker = "civ_spawn_idap";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "license_civ_idap";
+            };
+
+            class civ_aan {
+                displayName = "AAN World News";
+                spawnMarker = "civ_spawn_aan";
+                icon = "\a3\ui_f\data\map\LocationTypes\vegetationBroadleaf_CA.paa";
+                conditions = "license_civ_aan";
+            };*/
         };
     };
 };

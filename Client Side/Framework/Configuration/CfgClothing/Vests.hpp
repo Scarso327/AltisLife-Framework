9a3a327aea181af5f2223fe971a4e3795fbccbe5
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-22 01:51:03 +0000

    Added all vest classes

--- Client Side/Framework/Configuration/CfgClothing/Vests.hpp
+++ Client Side/Framework/Configuration/CfgClothing/Vests.hpp
@@ -1,4 +1,98 @@
-class V_TacVest_blk_POLICE {
-	price = 500;
+class V_LegStrapBag_black_F {
+	price = 0;
 	class Textures {};
 };
+class V_LegStrapBag_coyote_F : V_LegStrapBag_black_F{};
+class V_LegStrapBag_olive_F : V_LegStrapBag_black_F{};
+
+class V_Pocketed_black_F {
+	price = 0;
+	class Textures {};
+};
+class V_Pocketed_coyote_F : V_Pocketed_black_F{};
+class V_Pocketed_olive_F : V_Pocketed_black_F{};
+
+class V_Safety_yellow_F {
+	price = 0;
+	class Textures {};
+};
+class V_Safety_orange_F : V_Safety_yellow_F{};
+class V_Safety_blue_F : V_Safety_yellow_F{};
+
+class V_RebreatherB {
+	price = 0;
+	class Textures {};
+};
+class V_RebreatherIR : V_RebreatherB{};
+class V_RebreatherIA : V_RebreatherB{};
+
+class V_Rangemaster_vest {
+	price = 0;
+	class Textures {};
+};
+
+class V_BandollierB_blk {
+	price = 0;
+	class Textures {};
+};
+class V_BandollierB_cbr : V_BandollierB_blk{};
+class V_BandollierB_khk : V_BandollierB_blk{};
+class V_BandollierB_oli : V_BandollierB_blk{};
+class V_BandollierB_rgr : V_BandollierB_blk{};
+
+class V_HarnessO_brn {
+	price = 0;
+	class Textures {};
+};
+class V_HarnessO_gry : V_HarnessO_brn{};
+class V_HarnessOGL_brn : V_HarnessO_brn{};
+class V_HarnessOGL_gry : V_HarnessO_brn{};
+
+class V_TacVestIR_blk {
+	price = 0;
+	class Textures {};
+};
+
+class V_Chestrig_blk {
+	price = 0;
+	class Textures {};
+};
+class V_Chestrig_khk : V_Chestrig_blk{};
+class V_Chestrig_oli : V_Chestrig_blk{};
+class V_Chestrig_rgr : V_Chestrig_blk{};
+
+class V_TacChestrig_cbr_F {
+	price = 0;
+	class Textures {};
+};
+class V_TacChestrig_oli_F : V_TacChestrig_cbr_F{};
+class V_TacChestrig_grn_F : V_TacChestrig_cbr_F{};
+
+class V_Press_F {
+	price = 0;
+	class Textures {};
+};
+
+class V_TacVest_blk {
+	price = 0;
+	class Textures {};
+};
+class V_TacVest_blk_POLICE : V_TacVest_blk{};
+class V_TacVest_camo : V_TacVest_blk{};
+class V_TacVest_brn : V_TacVest_blk{};
+class V_TacVest_khk : V_TacVest_blk{};
+class V_TacVest_oli : V_TacVest_blk{};
+
+class V_PlateCarrier1_blk {
+	price = 0;
+	class Textures {};
+};
+class V_PlateCarrier1_rgr : V_PlateCarrier1_blk{};
+class V_PlateCarrier1_rgr_noflag_F : V_TacChestrig_cbr_F{};
+
+class V_PlateCarrier2_blk {
+	price = 0;
+	class Textures {};
+};
+class V_PlateCarrier2_rgr : V_PlateCarrier2_blk{};
+class V_PlateCarrier2_rgr_noflag_F : V_PlateCarrier2_blk{};

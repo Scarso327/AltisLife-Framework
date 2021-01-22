d482beb942b63f7a7cbf860a7072714a494ee79b
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-22 13:55:47 +0000

    Added vspace to vests

--- Client Side/Framework/Configuration/CfgClothing/Vests.hpp
+++ Client Side/Framework/Configuration/CfgClothing/Vests.hpp
@@ -1,5 +1,6 @@
 class V_LegStrapBag_black_F {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_LegStrapBag_coyote_F : V_LegStrapBag_black_F{};
@@ -7,6 +8,7 @@ class V_LegStrapBag_olive_F : V_LegStrapBag_black_F{};
 
 class V_Pocketed_black_F {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_Pocketed_coyote_F : V_Pocketed_black_F{};
@@ -14,6 +16,7 @@ class V_Pocketed_olive_F : V_Pocketed_black_F{};
 
 class V_Safety_yellow_F {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_Safety_orange_F : V_Safety_yellow_F{};
@@ -21,6 +24,7 @@ class V_Safety_blue_F : V_Safety_yellow_F{};
 
 class V_RebreatherB {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_RebreatherIR : V_RebreatherB{};
@@ -28,11 +32,13 @@ class V_RebreatherIA : V_RebreatherB{};
 
 class V_Rangemaster_vest {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 
 class V_BandollierB_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_BandollierB_cbr : V_BandollierB_blk{};
@@ -42,6 +48,7 @@ class V_BandollierB_rgr : V_BandollierB_blk{};
 
 class V_HarnessO_brn {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_HarnessO_gry : V_HarnessO_brn{};
@@ -50,11 +57,13 @@ class V_HarnessOGL_gry : V_HarnessO_brn{};
 
 class V_TacVestIR_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 
 class V_Chestrig_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_Chestrig_khk : V_Chestrig_blk{};
@@ -63,6 +72,7 @@ class V_Chestrig_rgr : V_Chestrig_blk{};
 
 class V_TacChestrig_cbr_F {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_TacChestrig_oli_F : V_TacChestrig_cbr_F{};
@@ -70,11 +80,13 @@ class V_TacChestrig_grn_F : V_TacChestrig_cbr_F{};
 
 class V_Press_F {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 
 class V_TacVest_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_TacVest_blk_POLICE : V_TacVest_blk{};
@@ -85,6 +97,7 @@ class V_TacVest_oli : V_TacVest_blk{};
 
 class V_PlateCarrier1_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_PlateCarrier1_rgr : V_PlateCarrier1_blk{};
@@ -92,6 +105,7 @@ class V_PlateCarrier1_rgr_noflag_F : V_TacChestrig_cbr_F{};
 
 class V_PlateCarrier2_blk {
 	price = 0;
+	virtualSpace = 0;
 	class Textures {};
 };
 class V_PlateCarrier2_rgr : V_PlateCarrier2_blk{};

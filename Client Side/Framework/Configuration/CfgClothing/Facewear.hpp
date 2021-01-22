78a212d95503f52905fe8663e83248401c9786da
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-22 13:59:34 +0000

    Added facewear checks

--- Client Side/Framework/Configuration/CfgClothing/Facewear.hpp
+++ Client Side/Framework/Configuration/CfgClothing/Facewear.hpp
@@ -1,27 +1,42 @@
 class G_WirelessEarpiece_F {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 
 class G_Aviator {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 
 class G_Squares {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Squares_Tinted : G_Squares{};
 
 class G_Spectacles {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Squares_Tinted : G_Spectacles{};
 
 class G_Shades_Black {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Shades_Red : G_Shades_Black{};
@@ -30,6 +45,9 @@ class G_Squares_Blue : G_Shades_Black{};
 
 class G_Sport_Red {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Shades_BlackWhite : G_Sport_Red{};
@@ -40,32 +58,51 @@ class G_Shades_checkered : G_Sport_Red{};
 
 class G_Lady_Blue {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 
 class G_Lowprofile {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 
 class G_Combat {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 
 class G_Tactical_Clear {
+	displayName = "Night Vision Glasses";
 	price = 0;
+	isMask = false;
+	isNV = true;
+	isGas = false;
 	class Textures {};
 };
 
 class G_EyeProtectors_F {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_EyeProtectors_Earpiece_F : G_EyeProtectors_F{};
 
 class G_B_Diving {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_O_Diving : G_B_Diving{};
@@ -73,6 +110,9 @@ class G_I_Diving : G_B_Diving{};
 
 class G_Respirator_white_F {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = true;
 	class Textures {};
 };
 class G_Respirator_blue_F : G_Respirator_white_F{};
@@ -80,17 +120,26 @@ class G_Respirator_yellow_F : G_Respirator_white_F{};
 
 class G_RegulatorMask_F {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = true;
 	class Textures {};
 };
 
 class G_Blindfold_01_black__F {
 	price = 0;
+	isMask = false;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Blindfold_01_white__F : G_Blindfold_01_black__F{};
 
 class G_Bandana_aviator {
 	price = 0;
+	isMask = true;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Bandana_shades : G_Bandana_aviator{};
@@ -102,6 +151,9 @@ class G_Respirator_oli : G_Bandana_aviator{};
 
 class G_Balaclava_blk {
 	price = 0;
+	isMask = true;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Balaclava_lowprofile : G_Balaclava_blk{};
@@ -110,6 +162,9 @@ class G_Balaclava_oli : G_Balaclava_blk{};
 
 class G_Balaclava_TI_blk_F {
 	price = 0;
+	isMask = true;
+	isNV = false;
+	isGas = false;
 	class Textures {};
 };
 class G_Balaclava_TI_G_blk_F : G_Balaclava_TI_blk_F{};

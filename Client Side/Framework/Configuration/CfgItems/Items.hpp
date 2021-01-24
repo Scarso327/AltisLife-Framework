380195fb99b1d6972e4cc0fbe7c7ccffb89968d4
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-24 03:45:07 +0000

    Added general items

---
+++ Client Side/Framework/Configuration/CfgItems/Items.hpp
@@ -0,0 +1,77 @@
+class ItemMap {
+	price = 0;
+	class Textures {};	
+};
+
+class ItemGPS {
+	price = 0;
+	class Textures {};	
+};
+
+class B_UavTerminal {
+	price = 0;
+	class Textures {};	
+};
+
+class ItemRadio {
+	price = 0;
+	class Textures {};	
+};
+
+class ItemCompass {
+	price = 0;
+	class Textures {};	
+};
+
+class ItemWatch {
+	price = 0;
+	class Textures {};	
+};
+
+class Binocular {
+	price = 0;
+	class Textures {};	
+};
+
+class Rangefinder {
+	price = 0;
+	class Textures {};	
+};
+
+class NVGoggles {
+	price = 0;
+	class Textures {};	
+};
+class NVGoggles_OPFOR : NVGoggles{};
+class NVGoggles_INDEP : NVGoggles{};
+
+class FirstAidKit {
+	price = 0;
+	class Textures {};	
+};
+
+class Medikit {
+	price = 0;
+	class Textures {};	
+};
+
+class ToolKit {
+	price = 0;
+	class Textures {};	
+};
+
+class HandGrenade_Stone {
+	price = 0;
+	class Textures {};	
+};
+
+class SmokeShell {
+	price = 0;
+	class Textures {};	
+};
+class SmokeShellRed : SmokeShell{};
+class SmokeShellGreen : SmokeShell{};
+class SmokeShellBlue : SmokeShell{};
+class SmokeShellOrange : SmokeShell{};
+class SmokeShellYellow : SmokeShell{};
+class SmokeShellPurple : SmokeShell{};

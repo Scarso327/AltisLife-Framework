b130c7c256c98dc61a0508fdb1288f0a788f40e8
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-24 03:14:22 +0000

    Added secondary weapons

--- Client Side/Framework/Configuration/CfgItems/Secondary.hpp
+++ Client Side/Framework/Configuration/CfgItems/Secondary.hpp
@@ -1,20 +1,44 @@
+class hgun_Pistol_01_F {
+	price = 0;
+	class Textures {};	
+};
+
+class hgun_Rook40_F {
+	price = 0;
+	class Textures {};	
+};
+
 class hgun_P07_F {
+	displayName = "Taser x26e"
 	price = 0;
 	class Textures {
 		class hgun_P07_F {
-			displayName = "Sand";
-			condition = "true";
+			displayName = "Khaki";
 		};
-
 		class hgun_P07_blk_F : hgun_P07_F {
 			displayName = "Black";
-		};
-
-		class hgun_P07_khk_F : hgun_P07_F {
-			displayName = "Khaki";
 		};
 	};	
 };
 
-class hgun_P07_blk_F : hgun_P07_F {};
-class hgun_P07_blk_Snds_F : hgun_P07_blk_F {};
+class hgun_ACPC2_F {
+	price = 0;
+	class Textures {};	
+};
+
+class hgun_Pistol_heavy_01_F {
+	price = 0;
+	class Textures {
+		class hgun_Pistol_heavy_01_F {
+			displayName = "Tan";
+		};
+		class hgun_Pistol_heavy_01_green_F : hgun_Pistol_heavy_01_F {
+			displayName = "Green";
+		};
+	};	
+};
+
+class hgun_Pistol_heavy_02_F {
+	price = 0;
+	class Textures {};	
+};

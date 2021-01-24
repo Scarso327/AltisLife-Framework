535a5d14285d50616eb29b45308bef6359995cca
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-01-24 02:48:51 +0000

    Added forgotten backpacks

--- Client Side/Framework/Configuration/CfgClothing/Backpacks.hpp
+++ Client Side/Framework/Configuration/CfgClothing/Backpacks.hpp
@@ -31,6 +31,7 @@ class B_AssaultPack_cbr : B_AssaultPack_blk{};
 class B_AssaultPack_khk : B_AssaultPack_blk{};
 class B_AssaultPack_rgr : B_AssaultPack_blk{};
 class B_AssaultPack_sgg : B_AssaultPack_blk{};
+class B_AssaultPack_wdl_F : B_AssaultPack_blk{};
 
 class B_FieldPack_blk {
 	price = 0;
@@ -71,6 +72,23 @@ class B_Carryall_cbr {
 class B_Carryall_khk : B_Carryall_cbr{};
 class B_Carryall_oli : B_Carryall_cbr{};
 class B_Carryall_green_F : B_Carryall_cbr{};
+class B_Carryall_wdl_F : B_Carryall_cbr{};
+
+class B_ViperLightHarness_blk_F {
+	price = 0;
+	virtualSpace = 0;
+	class Textures {};
+};
+class B_ViperLightHarness_khk_F : B_ViperLightHarness_blk_F{};
+class B_ViperLightHarness_oli_F : B_ViperLightHarness_blk_F{};
+
+class B_ViperHarness_blk_F {
+	price = 0;
+	virtualSpace = 0;
+	class Textures {};
+};
+class B_ViperHarness_khk_F : B_ViperHarness_blk_F{};
+class B_ViperHarness_oli_F : B_ViperHarness_blk_F{};
 
 class B_Bergen_mcamo_F {
 	price = 0;
@@ -79,3 +97,9 @@ class B_Bergen_mcamo_F {
 };
 class B_Bergen_dgtl_F : B_Bergen_mcamo_F{};
 class B_Bergen_hex_F : B_Bergen_mcamo_F{};
+
+class B_Parachute {
+	price = 0;
+	virtualSpace = 0;
+	class Textures {};
+};

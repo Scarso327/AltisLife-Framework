233beb41e736cc4b7ee8bd05de2575f8554ceeef
Jack Farhall <jackfarhall@gmail.com>
2021-02-08 21:43:40 +0000

    Updated : Virtual Items

--- Client Side/Framework/Configuration/CfgVirtualItems.hpp
+++ Client Side/Framework/Configuration/CfgVirtualItems.hpp
@@ -2,9 +2,61 @@ class CfgVirtualItems {
 	class Bottle {
 		displayName = "Water Bottle";
 		picture = "";
-		buyPrice = 0;
-		sellPrice = 0;
+		buyPrice = 100;
+		sellPrice = 20;
 		weight = 1;
+		class Settings {
+			isIllegal = false;
+			isSavable = true;
+		};
+		conditions = "true";
+	};
+
+	class RedApple {
+		displayName = "Red Apple";
+		picture = "";
+		buyPrice = 110;
+		sellPrice = 100;
+		weight = 2;
+		class Settings {
+			isIllegal = false;
+			isSavable = true;
+		};
+		conditions = "true";
+	};
+
+	class GreenApple {
+		displayName = "Green Apple";
+		picture = "";
+		buyPrice = 100;
+		sellPrice = 90;
+		weight = 2;
+		class Settings {
+			isIllegal = false;
+			isSavable = true;
+		};
+		conditions = "true";
+	};
+
+	class CopperOre {
+		displayName = "Copper Ore";
+		picture = "";
+		buyPrice = 2500;
+		sellPrice = 1350;
+		weight = 5;
+		class Settings {
+			isIllegal = false;
+			isSavable = false;
+		};
+		conditions = "true";
+	};
+
+	class Copper {
+		displayName = "Copper Ingot";
+		picture = "";
+		buyPrice = 3250;
+		sellPrice = 2250;
+		weight = 3;
 		class Settings {
 			isIllegal = false;
 			isSavable = true;

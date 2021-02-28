1250965f72d47a1fb4a1ac3a0b04bb12b376bbb2
Jack Farhall <jackfarhall@gmail.com>
2021-02-28 16:22:00 +0000

    CfgLicenses Format

--- Client Side/Framework/Configuration/CfgLicenses.hpp
+++ Client Side/Framework/Configuration/CfgLicenses.hpp
@@ -1,146 +1,19 @@
 class CfgLicenses {
-    // Driver and Vehicle Licensing Agency
-    class driving {
-        variable = "driving";
+    class BaseLicense {
+        buyPrice = 0;
+        factions[] = { "Civilian" };
+        reqLicenses[] = {};
+        class Settings {
+            isVisible = true; // Whether it's displayed when your licenses are checked...
+            isRemovable = false; // Whether it can be removed by Police...
+        };
+        conditions = "true";
+    };
+
+    class Driving : BaseLicense {
         displayName = "Driving Licence";
-        price = 0;
-        removable = true;
-    };
-
-    class trucking {
-        variable = "trucking";
-        displayName = "Trucking Licence";
-        price = 0;
-        removable = true;
-    };
-
-    class piloting {
-        variable = "piloting";
-        displayName = "Piloting Licence";
-        price = 0;
-        removable = true;
-    };
-
-    class boating {
-        variable = "boating";
-        displayName = "Boating Licence";
-        price = 0;
-        removable = true;
-    };
-
-    // Property Agents
-    class homeowners {
-        variable = "homeowners";
-        displayName = "Homeowner's Approval";
-        price = 0;
-        removable = true;
-    };
-
-    // Handgun Store
-    class firearms {
-        variable = "firearms";
-        displayName = "Firearms Certification";
-        price = 0;
-        removable = true;
-    };
-
-    // Item Processing Sites
-    class lumber {
-        variable = "lumber";
-        displayName = "Lumber Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class schnapps {
-        variable = "schnapps";
-        displayName = "Schnapps Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class tin {
-        variable = "tin";
-        displayName = "Tin Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class copper {
-        variable = "copper";
-        displayName = "Copper Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class iron {
-        variable = "iron";
-        displayName = "Iron Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class silver {
-        variable = "silver";
-        displayName = "Silver Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class oil {
-        variable = "oil";
-        displayName = "Oil Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class gem {
-        variable = "gem";
-        displayName = "Gem Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class marijuana {
-        variable = "marijuana";
-        displayName = "Marijuana Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class heroin {
-        variable = "heroin";
-        displayName = "Heroin Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class cocaine {
-        variable = "cocaine";
-        displayName = "Cocaine Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class methamphetamine {
-        variable = "methamphetamine";
-        displayName = "Methamphetamine Processing Licence";
-        price = 0;
-        removable = false;
-    };
-
-    class drug_refining {
-        variable = "drug_refining";
-        displayName = "Drub Refining Licence";
-        price = 0;
-        removable = false;
-    };
-
-    // Syndikat Outpost
-    class syndikat {
-        variable = "syndikat";
-        displayName = "Syndikat Membership";
-        price = 0;
-        removable = false;
+        class Settings {
+            isRemovable = true;
+        };
     };
 };

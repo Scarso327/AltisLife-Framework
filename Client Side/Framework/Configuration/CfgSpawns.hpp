8253899f46817af9110a23242c4ba81223b633e1
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-01 01:58:59 +0000

    Update CfgSpawns.hpp

--- Client Side/Framework/Configuration/CfgSpawns.hpp
+++ Client Side/Framework/Configuration/CfgSpawns.hpp
@@ -5,13 +5,85 @@ class CfgSpawns {
         radius = 300;
         conditions = "true";
     };
+    class Police : BaseSpawn { factions[] = { "Police" }; };
+    class Medic : BaseSpawn { factions[] = { "Medic" }; };
 
-    class Police : BaseSpawn {
-        factions[] = { "Police" };
-    };
+    class Altis {
+        // Altis Police Constabulary
+        class KavalaStation : Police {
+            displayName = "Kavala Station";
+            marker = "apc_spawn_kavala";
+            icon = "data\icons\ico_Kavala.paa";
+        };
+        class AthiraStation : Police {
+            displayName = "Athira Station";
+            marker = "apc_spawn_athira";
+            icon = "data\icons\ico_Athira.paa";
+        };
+        class PyrgosStation : Police {
+            displayName = "Pyrgos Station";
+            marker = "apc_spawn_pyrgos";
+            icon = "data\icons\ico_Pyrgos.paa";
+        };
+        class AgiosStation : Police {
+            displayName = "Agios Station";
+            marker = "apc_spawn_agios";
+            icon = "data\icons\ico_Agios.paa";
+        };
+        class MarineStation : Police {
+            displayName = "Marine Base";
+            marker = "apc_spawn_marine";
+            icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
+        };
 
-    class NHS : BaseSpawn {
-        factions[] = { "Medic" };
+        // National Health Service
+        class KavalaHospital : Medic {
+            displayName = "Kavala Hospital";
+            marker = "nhs_spawn_kavala";
+            icon = "data\icons\ico_Kavala.paa";
+        };
+        class AthiraHospital : Medic {
+            displayName = "Athira Hospital";
+            marker = "nhs_spawn_athira";
+            icon = "data\icons\ico_Athira.paa";
+        };
+        class PyrgosHospital : Medic {
+            displayName = "Pyrgos Hospital";
+            marker = "nhs_spawn_pyrgos";
+            icon = "data\icons\ico_Pyrgos.paa";
+        };
+
+        // Civilian
+        class Kavala : BaseSpawn {
+            displayName = "Kavala";
+            marker = "civ_spawn_kavala";
+            icon = "data\icons\ico_Kavala.paa";
+        };
+        class Athira : BaseSpawn {
+            displayName = "Athira";
+            marker = "civ_spawn_athira";
+            icon = "data\icons\ico_Athira.paa";
+        };
+        class Pyrgos : BaseSpawn {
+            displayName = "Pyrgos";
+            marker = "civ_spawn_pyrgos";
+            icon = "data\icons\ico_Pyrgos.paa";
+        };
+        class Sofia : BaseSpawn {
+            displayName = "Sofia";
+            marker = "civ_spawn_sofia";
+            icon = "data\icons\ico_Agios.paa"; // CHANGE
+        };
+        class Agios : BaseSpawn {
+            displayName = "Agios";
+            marker = "civ_spawn_agios";
+            icon = "data\icons\ico_Agios.paa"; // CHANGE
+        };
+        class Neochori : BaseSpawn {
+            displayName = "Neochori";
+            marker = "civ_spawn_neochori";
+            icon = "data\icons\ico_Agios.paa"; // CHANGE
+        };
     };
 
     class Malden {

7ab941ff69fdd7d8f57d0bc9998e694fd43eadcc
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-01 15:00:57 +0000

    Update Magazines.hpp

--- Client Side/Framework/Configuration/CfgItems/Magazines.hpp
+++ Client Side/Framework/Configuration/CfgItems/Magazines.hpp
@@ -1,20 +1,211 @@
-class 30Rnd_65x39_caseless_mag {
-	price = 500;
+class 6Rnd_RedSignal_F {
+	displayName = "6Rnd Signal Cylinder";
+	price = 0;
 	class Textures {
-		class 30Rnd_65x39_caseless_mag {
-			displayName = "Sand";
+		class 6Rnd_RedSignal_F {
+			displayName = "Red";
 			condition = "true";
 		};
-
-		class 30Rnd_65x39_caseless_black_mag : 30Rnd_65x39_caseless_mag {
-			displayName = "Black";
-		};
-
-		class 30Rnd_65x39_caseless_khaki_mag : 30Rnd_65x39_caseless_mag {
-			displayName = "Khaki";
-		};
+		class 6Rnd_GreenSignal_F : 6Rnd_RedSignal_F { displayName = "Green"; };
 	};
 };
+class 6Rnd_GreenSignal_F : 6Rnd_RedSignal_F {};
 
-class 30Rnd_65x39_caseless_black_mag : 30Rnd_65x39_caseless_mag {};
-class 30Rnd_65x39_caseless_khaki_mag : 30Rnd_65x39_caseless_mag {};
+// 9mm
+class 10Rnd_9x21_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 16Rnd_9x21_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_9x21_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_9x21_Mag_SMG_02 {
+	price = 0;
+	class Textures {};
+};
+
+// .45 ACP
+class 9Rnd_45ACP_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 11Rnd_45ACP_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_45ACP_Mag_SMG_01 {
+	price = 0;
+	class Textures {};
+};
+
+class 6Rnd_45ACP_Cylinder {
+	price = 0;
+	class Textures {};
+};
+
+// 50BW
+class 10Rnd_50BW_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+// 12 Gauge
+class 2Rnd_12Gauge_Pellets {
+	price = 0;
+	class Textures {};
+};
+
+class 6Rnd_12Gauge_Pellets {
+	price = 0;
+	class Textures {};
+};
+
+class 2Rnd_12Gauge_Slug {
+	price = 0;
+	class Textures {};
+};
+
+class 6Rnd_12Gauge_Slug {
+	price = 0;
+	class Textures {};
+};
+
+// 5.45mm
+class 30Rnd_545x39_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+// 5.7mm
+class 50Rnd_570x28_SMG_03 {
+	price = 0;
+	class Textures {};
+};
+
+// 5.56mm
+class 20Rnd_556x45_UW_mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_556x45_Stanag {
+	price = 0;
+	class Textures {};
+};
+
+class 150Rnd_556x45_Drum_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+class 200Rnd_556x45_Box_F {
+	price = 0;
+	class Textures {};
+};
+
+// 5.8mm
+class 30Rnd_580x42_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+class 100Rnd_580x42_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+// 6.5mm
+class 20Rnd_650x39_Cased_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_65x39_caseless_green {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_65x39_caseless_msbs_mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_65x39_caseless_black_mag {
+	price = 0;
+	class Textures {};
+};
+
+class 100Rnd_65x39_caseless_black_mag {
+	price = 0;
+	class Textures {};
+};
+
+class 200Rnd_65x39_cased_Box {
+	price = 0;
+	class Textures {};
+};
+
+// 7.62mm
+class 10Rnd_762x54_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 10Rnd_Mk14_762x51_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 20Rnd_762x51_Mag {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_762x39_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+class 30Rnd_762x39_AK12_Mag_F {
+	price = 0;
+	class Textures {};
+};
+
+class 150Rnd_762x54_Box {
+	price = 0;
+	class Textures {};
+};
+
+// 9.3mm
+class 10Rnd_93x64_DMR_05_Mag {
+	price = 0;
+	class Textures {};
+};
+
+// 12.7mm
+class 10Rnd_127x54_Mag {
+	price = 0;
+	class Textures {};
+};
+
+// .338mm
+class 10Rnd_338_Mag {
+	price = 0;
+	class Textures {};
+};
+
+// .408mm
+class 7Rnd_408_Mag {
+	price = 0;
+	class Textures {};
+};

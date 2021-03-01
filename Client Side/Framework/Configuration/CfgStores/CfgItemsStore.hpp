2286f3ec5f5cb1d50ea4562e3c224ab475cd8900
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-01 15:54:55 +0000

    Update CfgItemsStore.hpp

--- Client Side/Framework/Configuration/CfgStores/CfgItemsStore.hpp
+++ Client Side/Framework/Configuration/CfgStores/CfgItemsStore.hpp
@@ -148,9 +148,7 @@ class CfgItemsStore {
 			class optic_Hamr {
 				price = 0;
 				condition = "[""Police_Main"", 2] call ULP_fnc_hasAccess";
-				class textures {
-					class optic_Hamr {};
-				};
+				class textures { class optic_Hamr {}; };
 			};
 			class optic_MRCO { 
 				price = 0;
@@ -159,9 +157,7 @@ class CfgItemsStore {
 			class optic_Arco {
 				price = 0;
 				condition = "[""Police_Main"", 2] call ULP_fnc_hasAccess";
-				class textures {
-					class optic_Arco_blk_F {};
-				};
+				class textures { class optic_Arco_blk_F {}; };
 			};
 			class optic_ERCO_blk_F { 
 				price = 0;
@@ -170,30 +166,17 @@ class CfgItemsStore {
 			class optic_DMS {
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 2] call ULP_fnc_hasAccess }";
-				class textures {
-					class optic_DMS {};
-				};
+				class textures { class optic_DMS {}; };
 			};
 			class optic_SOS {
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 2] call ULP_fnc_hasAccess }";
-				class textures {
-					class optic_SOS {};
-				};
+				class textures { class optic_SOS {}; };
 			};
 			class optic_AMS {
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 3] call ULP_fnc_hasAccess }";
-				class textures {
-					class optic_AMS {};
-				};
-			};
-			class optic_LRPS {
-				price = 0;
-				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 3] call ULP_fnc_hasAccess }";
-				class textures {
-					class optic_LRPS {};
-				};
+				class textures { class optic_AMS {}; };
 			};
 			class optic_NVS { 
 				price = 0;
@@ -212,9 +195,7 @@ class CfgItemsStore {
 			class muzzle_snds_M { 
 				price = 0;
 				condition = "[""Police_Main"", 3] call ULP_fnc_hasAccess || { [""Police_SFO"", 1] call ULP_fnc_hasAccess }"; 
-				class textures {
-					class muzzle_snds_M {};
-				};
+				class textures { class muzzle_snds_M {}; };
 			};
 			class muzzle_snds_570 { 
 				price = 0;
@@ -223,30 +204,22 @@ class CfgItemsStore {
 			class muzzle_snds_58_blk_F { 
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 1] call ULP_fnc_hasAccess }"; 
-				class textures {
-					class muzzle_snds_58_blk_F {};
-				};
+				class textures { class muzzle_snds_58_blk_F {}; };
 			};
 			class muzzle_snds_H { 
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 1] call ULP_fnc_hasAccess }"; 
-				class textures {
-					class muzzle_snds_H {};
-				};
+				class textures { class muzzle_snds_H {}; };
 			};
 			class muzzle_snds_65_TI_blk_F { 
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 1] call ULP_fnc_hasAccess }"; 
-				class textures {
-					class muzzle_snds_65_TI_blk_F {};
-				};
+				class textures { class muzzle_snds_65_TI_blk_F {}; };
 			};
 			class muzzle_snds_B { 
 				price = 0;
 				condition = "[""Police_Main"", 10] call ULP_fnc_hasAccess || { [""Police_SFO"", 2] call ULP_fnc_hasAccess }"; 
-				class textures {
-					class muzzle_snds_B {};
-				};
+				class textures { class muzzle_snds_B {}; };
 			};
 		};
 		class Magazines {};
@@ -286,9 +259,7 @@ class CfgItemsStore {
 			class SmokeShell { 
 				price = 0;
 				condition = "true"; 
-				class textures {
-					class SmokeShell {};
-				};
+				class textures { class SmokeShell {}; };
 			};
 		};
 	};
@@ -300,7 +271,6 @@ class CfgItemsStore {
 		class Pistols {};
 		class Attachments {};
 		class Magazines {};
-
 		class Items {
 			class ItemMap {};
 			class ItemCompass {};
@@ -319,7 +289,6 @@ class CfgItemsStore {
 		class Pistols {};
 		class Attachments {};
 		class Magazines {};
-
 		class Items {
 			class ItemMap {};
 			class ItemCompass {};
@@ -358,9 +327,7 @@ class CfgItemsStore {
 		condition = "true";
 		class Rifles {
 			class sgun_HunterShotgun_01_sawedoff_F {
-				class textures {
-					class sgun_HunterShotgun_01_F {};
-				};
+				class textures { class sgun_HunterShotgun_01_F {}; };
 			};
 			class srifle_DMR_06_hunter_F {};
 		};
@@ -396,9 +363,7 @@ class CfgItemsStore {
 			class arifle_TRG20_F {};
 			class arifle_Mk20_plain_F {};
 			class arifle_SPAR_01_blk_F {};
-			class arifle_SPAR_02_blk_F {};
 			class arifle_CTAR_blk_F {};
-			class arifle_CTARS_blk_F {};
 			class srifle_DMR_07_blk_F {};
 			class arifle_Katiba_C_F {};
 			class arifle_MSBS65_black_F {};
@@ -428,7 +393,28 @@ class CfgItemsStore {
 			class optic_Arco {};
 			class optic_ERCO_blk_F {};
 		};
-		class Magazines {};
+		class Magazines {
+			class 10Rnd_9x21_Mag {};
+			class 16Rnd_9x21_Mag {};
+			class 30Rnd_9x21_Mag {};
+			class 30Rnd_9x21_Mag_SMG_02 {};
+			class 9Rnd_45ACP_Mag {};
+			class 11Rnd_45ACP_Mag {};
+			class 6Rnd_45ACP_Cylinder {};
+			class 30Rnd_45ACP_Mag_SMG_01 {};
+			class 2Rnd_12Gauge_Pellets {};
+			class 2Rnd_12Gauge_Slug {};
+			class 30Rnd_545x39_Mag_F {};
+			class 50Rnd_570x28_SMG_03 {};
+			class 20Rnd_556x45_UW_mag  {};
+			class 30Rnd_556x45_Stanag {};
+			class 30Rnd_580x42_Mag_F {};
+			class 20Rnd_650x39_Cased_Mag_F {};
+			class 30Rnd_65x39_caseless_green {};
+			class 30Rnd_65x39_caseless_msbs_mag {};
+			class 30Rnd_65x39_caseless_black_mag {};
+			class 30Rnd_762x39_Mag_F {};
+		};
 		class Items {
 			class ItemMap {};
 			class ItemCompass {};
@@ -458,6 +444,7 @@ class CfgItemsStore {
 			class arifle_Mk20_plain_F {};
 			class arifle_SPAR_01_blk_F {};
 			class arifle_SPAR_02_blk_F {};
+			class LMG_03_F {};
 			class arifle_CTAR_blk_F {};
 			class arifle_CTARS_blk_F {};
 			class srifle_DMR_07_blk_F {};
@@ -484,30 +471,48 @@ class CfgItemsStore {
 			class hgun_Pistol_heavy_02_F {};
 		};
 		class Attachments {
-			class hgun_PDW2000_F {};
-			class SMG_02_F {};
-			class SMG_01_F {};
-			class sgun_HunterShotgun_01_sawedoff_F {};
-			class SMG_03C_black {};
-			class SMG_03_black {};
-			class arifle_AKS_F {};
-			class arifle_SDAR_F {};
-			class arifle_TRG20_F {};
-			class arifle_Mk20_plain_F {};
-			class arifle_SPAR_01_blk_F {};
-			class arifle_SPAR_02_blk_F {};
-			class arifle_CTAR_blk_F {};
-			class arifle_CTARS_blk_F {};
-			class srifle_DMR_07_blk_F {};
-			class arifle_Katiba_C_F {};
-			class arifle_MSBS65_black_F {};
-			class arifle_MSBS65_Mark_black_F {};
-			class arifle_MXC_Black_F {};
-			class arifle_MX_Black_F {};
-			class arifle_MXM_Black_F {};
-			class arifle_AKM_F {};
+			class bipod_01_F_blk {};
+			class acc_flashlight {};
+			class acc_flashlight_pistol {};
+			class acc_pointer_IR {};
+			class optic_MRD {};
+			class optic_Aco {};
+			class optic_Holosight {};
+			class optic_Hamr {};
+			class optic_MRCO {};
+			class optic_Arco {};
+			class optic_ERCO_blk_F {};
+			class optic_DMS {};
+			class optic_SOS {};
 		};
-		class Magazines {};
+		class Magazines {
+			class 10Rnd_9x21_Mag {};
+			class 16Rnd_9x21_Mag {};
+			class 30Rnd_9x21_Mag {};
+			class 30Rnd_9x21_Mag_SMG_02 {};
+			class 9Rnd_45ACP_Mag {};
+			class 11Rnd_45ACP_Mag {};
+			class 6Rnd_45ACP_Cylinder {};
+			class 30Rnd_45ACP_Mag_SMG_01 {};
+			class 2Rnd_12Gauge_Pellets {};
+			class 2Rnd_12Gauge_Slug {};
+			class 30Rnd_545x39_Mag_F {};
+			class 50Rnd_570x28_SMG_03 {};
+			class 20Rnd_556x45_UW_mag  {};
+			class 30Rnd_556x45_Stanag {};
+			class 150Rnd_556x45_Drum_Mag_F {};
+			class 200Rnd_556x45_Box_F {};
+			class 30Rnd_580x42_Mag_F {};
+			class 100Rnd_580x42_Mag_F {};
+			class 20Rnd_650x39_Cased_Mag_F {};
+			class 30Rnd_65x39_caseless_green {};
+			class 30Rnd_65x39_caseless_msbs_mag {};
+			class 30Rnd_65x39_caseless_black_mag {};
+			class 100Rnd_65x39_caseless_black_mag {};
+			class 10Rnd_762x54_Mag {};
+			class 20Rnd_762x51_Mag {};
+			class 30Rnd_762x39_Mag_F {};
+		};
 		class Items {
 			class ItemMap {};
 			class ItemCompass {};

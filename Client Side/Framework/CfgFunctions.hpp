2bc3f88927dd1848fb4f06e9abd231c613fc06c8
Jack Farhall <jackfarhall@gmail.com>
2021-03-04 23:39:48 +0000

    Moved : Inventory script to Inventory folder

--- Client Side/Framework/CfgFunctions.hpp
+++ Client Side/Framework/CfgFunctions.hpp
@@ -79,7 +79,6 @@ class ULP_Functions {
 		class isKnocked {};
 		class initSurvival {};
 		class initPlayerEvents {};
-		class getInventory {};
 		class resetSurvival {};
 		class canRestrain {};
 		class getName {};
@@ -170,6 +169,7 @@ class ULP_Functions {
 		class setGear {};
 		class addItems {};
 		class stripGear {};
+		class getInventory {};
 	};
 	
 	class Gathering {

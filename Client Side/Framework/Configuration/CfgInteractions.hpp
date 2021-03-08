36001901570105c5d697e83b1bf5305f78b6d4fe
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-08 11:09:37 +0000

    Update CfgInteractions.hpp

--- Client Side/Framework/Configuration/CfgInteractions.hpp
+++ Client Side/Framework/Configuration/CfgInteractions.hpp
@@ -1,16 +1,92 @@
 class CfgInteractions {
+	/* GENERAL INTERACTIONS */
+	// Show ID	
+	// Show Licenses	
+	// Give Cash
+	// Give Keys
+
+	/* RESTRAINED INTERACTIONS */
+	// Unrestrain
+	// Escort/Stop Escorting
+	// Put In Vehicle
+	// Feed/Hydrate
+	// Blindfold/Remove Blindfold
+	// Gag/Remove Gag
+	// Check Inventory
+	// Check vItems
+	// Check ID
+	// Check Licenses
+
+	/* POLICE INTERACTIONS */
+	// Issue Ticket
+	// Send to Prison
+
+	/* MEDIC INTERACTIONS */
+	// Revive
+
 	class Vehicle {
-		class Repair {
-			title = "Repair";
-			onClick = "_this call ULP_fnc_repairVehicle";
-			factions[] = { "Police", "Medic", "Civilian" };
-			condition = "true";
+		class VehicleBase {
+			factions[] = { "Police", "Medic", "Hato", "Civilian" };
+			condition = "ture";
 		};
 
-		class Impound : Repair {
-			title = "Impound";
+		class Repair : VehicleBase {
+			title = "Repair";
+			onClick = "_this call ULP_fnc_repairVehicle";
+		};
+
+		class Lockpick : VehicleBase {
+			title = "Lockpick";
+			onClick = "_this call ULP_fnc_repairVehicle";
+		};
+
+		class Registration : VehicleBase {
+			title = "Registration";
+			onClick = "hint ""Hello"";";
+			factions[] = { "Police", "Hato" };
+		};
+
+		class Clamp : VehicleBase {
+			title = "Clamp";
+			onClick = "_this call ULP_fnc_repairVehicle";
+			factions[] = { "Police", "Hato" };
+		};
+
+		class Search : VehicleBase {
+			title = "Search";
 			onClick = "hint ""Hello"";";
 			factions[] = { "Police" };
+		};
+
+		class Garage : VehicleBase {
+			title = "Garage";
+			onClick = "hint ""Hello"";";
+			factions[] = { "Police", "Hato" };
+			condition = "[""Police_Main"", 2] call ULP_fnc_hasAccess || { [""Medic_Main"", 2] call ULP_fnc_hasAccess }";
+		};
+
+		class Impound : VehicleBase {
+			title = "Impound";
+			onClick = "hint ""Hello"";";
+			factions[] = { "Police", "Hato" };
+			condition = "[""Police_Main"", 3] call ULP_fnc_hasAccess";
+		};
+
+		class Scrap : VehicleBase {
+			title = "Scrap";
+			onClick = "hint ""Hello"";";
+			factions[] = { "Police" };
+			condition = "[""Police_Main"", 3] call ULP_fnc_hasAccess";
+		};
+
+		class PulloutOccupants : VehicleBase {
+			title = "Pullout Occupants";
+			onClick = "hint ""Hello"";";
+		};
+
+		class Push : VehicleBase {
+			title = "Push";
+			onClick = "hint ""Hello"";";
 		};
 	};
 };

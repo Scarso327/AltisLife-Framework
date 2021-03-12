8b0aa2b61204298e4bbafa8e7e76b932e4209a81
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-12 02:11:06 +0000

    Update CfgGathering.hpp

--- Client Side/Framework/Configuration/CfgGathering.hpp
+++ Client Side/Framework/Configuration/CfgGathering.hpp
@@ -1,81 +1,80 @@
 class CfgGathering {
 	class BaseGather {
+		actionTitle = "Gathering";
 		gatherTime = 4;
 		factions[] = { "Civilian", "Police", "Medic", "Hato" };
 		reqItems[] = {};
 		reqLicenses[] = {};
-		profession[] = { "Gathering", 1, 30 }; // PROFESSION, INCREASE AMOUNT, CHANCE OF INCREASE
+		profession[] = { "Gathering", 1, 100 };
+	};
+
+	class BaseMine : BaseGather {
+		actionTitle = "Mining";
+		profession[] = { "Mining", 1, 100 };
 	};
 
 	class Apple : BaseGather {
 		areas[] = { { "apple_field_1", 20 }, { "apple_field_2", 20 }, { "apple_field_3", 20 }, { "apple_field_4", 20 } };
-		items[] = { { "redApple", 50 }, { "greenApple", 20 } };
+		items[] = { { "Apple", 100 } };
 	};
 
 	class Peach : BaseGather {
 		areas[] = { { "peach_field_1", 20 }, { "peach_field_2", 20 }, { "peach_field_3", 20 }, { "peach_field_4", 20 } };
-		items[] = { { "peach", 100 } };
+		items[] = { { "Peach", 100 } };
 	};
 
 	class Tobacco : BaseGather {
 		areas[] = { { "tobacco_field", 20 } };
-		items[] = { { "tobacco", 100 } };
+		items[] = { { "Tobacco", 100 } };
 	};
 
 	class Weed : BaseGather {
 		areas[] = { { "weed_field", 20 } };
-		items[] = { { "unprocessedWeed", 100 } };
+		items[] = { { "UnprocessedWeed", 100 } };
 	};
 
 	class Heroin : BaseGather {
 		areas[] = { { "heroin_field", 20 } };
-		items[] = { { "unprocessedHeroin", 100 } };
+		items[] = { { "UnprocessedHeroin", 100 } };
 	};
 
 	class Cocaine : BaseGather {
 		areas[] = { { "cocaine_field", 20 } };
-		items[] = { { "unprocessedCocaine", 100 } };
+		items[] = { { "UnprocessedCocaine", 100 } };
 	};
 
-	class Copper : BaseGather {
+	class Copper : BaseMine {
 		areas[] = { { "copper_mine", 20 } };
-		items[] = { { "copperOre", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "CopperOre", 100 } };
 	};
 
-	class Silver : BaseGather {
+	class Silver : BaseMine {
 		areas[] = { { "silver_mine", 20 } };
-		items[] = { { "silverOre", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "SilverOre", 100 } };
 	};
 
-	class Iron : BaseGather {
+	class Iron : BaseMine {
 		areas[] = { { "iron_mine", 20 } };
-		items[] = { { "ironOre", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "IronOre", 100 } };
 	};
 
-	class Sand : BaseGather {
+	class Sand : BaseMine {
 		areas[] = { { "sand_mine", 20 } };
-		items[] = { { "sand", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "Sand", 100 } };
 	};
 
-	class Salt : BaseGather {
+	class Salt : BaseMine {
 		areas[] = { { "salt_mine", 20 } };
-		items[] = { { "unrefinedSalt", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "UnrefinedSalt", 100 } };
 	};
 
-	class Diamond : BaseGather {
+	class Diamond : BaseMine {
 		areas[] = { { "diamond_mine", 20 } };
-		items[] = { { "uncutDiamond", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "UncutDiamond", 100 } };
 	};
 
-	class Uranium : BaseGather {
+	class Uranium : BaseMine {
 		areas[] = { { "uranium_mine", 20 } };
-		items[] = { { "uraniumOre", 100 } };
-		actionTitle = "Mining";
+		items[] = { { "UraniumOre", 100 } };
 	};
 };

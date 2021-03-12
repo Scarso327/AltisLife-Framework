d04174f976f253b5a5e34dc5cda9745de66e3e60
Jack Farhall <jackfarhall@gmail.com>
2021-03-12 01:59:19 +0000

    Update CfgProfessions.hpp

--- Client Side/Framework/Configuration/CfgProfessions.hpp
+++ Client Side/Framework/Configuration/CfgProfessions.hpp
@@ -1,6 +1,8 @@
 class CfgProfessions {
 	class Repairing {
 		displayName = "Repairing";
+		icon = "";
+		factions[] = { "Police", "Medic", "Civilian" };
 		effectDescription = "Currently decreases the time it takes to repair by %1%2";
 		effectCalculation = "5 * (_this select 0)"; // (_this select 0) refers to the level of this profession and (_this select 1) is the current xp.
 		xpLevel = 100;

205fbdaf8bdc7d84d085c683031715f196b9a9e7
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-13 22:02:21 +0000

    Update CfgAchievements.hpp

--- Client Side/Framework/Configuration/CfgAchievements.hpp
+++ Client Side/Framework/Configuration/CfgAchievements.hpp
@@ -7,6 +7,86 @@ class CfgAchievements {
 		isMystery = false;
 	};
 
+	class JoinGroup {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Member";
+		description = "Joined a group for the first time.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class JoinAPC {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Constable";
+		description = "Joined the Altis Police Constabulary.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class JoinNHS {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Paramedic";
+		description = "Joined the National Health Service.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class JoinHATO {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Traffic Officer";
+		description = "Joined the Highways Agency.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class Governor {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Governor";
+		description = "Win an election and become the Governor.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class Employment {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Employment";
+		description = "Successfully become employemented.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class Convict {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Convict";
+		description = "Have been sent to prison by a constable.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class TaskMaster {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Task Master";
+		description = "Completed a task for the first time.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class LifeSaver {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Life Saver";
+		description = "Sucessfully revived another person.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class Cartel {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "Cartel Lord";
+		description = "Sucessfully capture a cartel.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
 	class BuyVehicle {
 		icon = "Data\UI\repairing_prof.paa";
 		displayName = "First Ride";
@@ -23,10 +103,34 @@ class CfgAchievements {
 		isMystery = false;
 	};
 
+	class BuyWarehouse {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "First Warehouse";
+		description = "Bought your first warehouse.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
 	class OneMillion {
 		icon = "Data\UI\repairing_prof.paa";
 		displayName = "£1,000,000";
 		description = "Had a total of £1,000,000 in your bank.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class TenMillion {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "£10,000,000";
+		description = "Had a total of £10,000,000 in your bank.";
+		leveling[] = { 50, "Achievement Unlocked", 100 };
+		isMystery = false;
+	};
+
+	class HundredMillion {
+		icon = "Data\UI\repairing_prof.paa";
+		displayName = "£100,000,000";
+		description = "Had a total of £100,000,000 in your bank.";
 		leveling[] = { 50, "Achievement Unlocked", 100 };
 		isMystery = false;
 	};
@@ -36,7 +140,7 @@ class CfgAchievements {
 		displayName = "Prestigious";
 		description = "Prestiged for the first time.";
 		leveling[] = { 50, "Achievement Unlocked", 100 };
-		isMystery = true;
+		isMystery = false;
 	};
 
 	class FanGirl {

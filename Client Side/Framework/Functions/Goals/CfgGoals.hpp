2818b577d66698dca12108f1da1049d6b6a6844a
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-03-24 03:19:41 +0000

    Update CfgGoals.hpp

--- Client Side/Framework/Functions/Goals/CfgGoals.hpp
+++ Client Side/Framework/Functions/Goals/CfgGoals.hpp
@@ -1,91 +1,150 @@
 class CfgGoals {
 	class Personal {
 		class Daily {
-			Amount = 5;
+			Amount = 3;
 
-			class GatherCopper {
+			class GatherApple {
 				type = "GATHERING";
+				item = "FreshApple";
+				title = "Gather Apples";
+				description = "Gather Fresh Apples before today's end!";
+				tiers[] = { 250, 500, 1000 };
+				condition = "true";
+			};
+			class GatherPeach : GatherApple {
+				item = "FreshPeach";
+				title = "Gather Peaches";
+				description = "Gather Fresh Peachs before today's end!";
+			};
+			class GatherCoral : GatherApple {
+				item = "Coral";
+				title = "Gather Peaches";
+				description = "Gather Coral before today's end!";
+			};
+			class GatherCorn : GatherApple {
+				item = "Corn";
+				title = "Gather Corn";
+				description = "Gather Corn before today's end!";
+			};
+
+			class MineCoal {
+				type = "GATHERING";
+				item = "Coal";
+				title = "Mine Coal";
+				description = "Mine Coal before today's end!";
+				tiers[] = { 100, 300, 750 };
+				condition = "true";
+			};
+			class MineCopper : MineCoal {
 				item = "CopperOre";
-				title = "Copper Kings";
-				description = "Mine Copper Ore before todays end!";
-				tiers[] = { 300, 500, 700 };
-				condition = "true";
+				title = "Mine Copper";
+				description = "Mine Copper before today's end!";
 			};
-
-			class GatherSilver {
-				type = "GATHERING";
+			class MineSilver : MineCoal {
 				item = "SilverOre";
-				title = "Silverware";
-				description = "Mine Silver Ore before todays end!";
-				tiers[] = { 300, 500, 700 };
-				condition = "true";
+				title = "Mine Silver";
+				description = "Mine Silver before today's end!";
 			};
-
-			class GatherIron {
-				type = "GATHERING";
+			class MineIron : MineCoal {
 				item = "IronOre";
-				title = "Ironman";
-				description = "Mine Iron Ore before todays end!";
-				tiers[] = { 300, 500, 700 };
-				condition = "true";
+				title = "Mine Iron";
+				description = "Mine Iron before today's end!";
 			};
 
-			class SellCopper {
-				type = "SELLING";
-				item = "CopperBar";
-				title = "Seat of Copper";
-				description = "Sell Copper Bar(s) before todays end!";
-				tiers[] = { 100, 150, 300 };
+			class ProcessTobacco {
+				type = "PROCESSING";
+				item = "Tobacco";
+				title = "Process Tobacco";
+				description = "Process Tobacco before today's end!";
+				tiers[] = { 100, 250, 500 };
 				condition = "true";
 			};
-
-			class SellSilver {
-				type = "SELLING";
-				item = "SilverOre";
-				title = "Silver Rings";
-				description = "Sell Silver Bar(s) before todays end!";
-				tiers[] = { 100, 150, 300 };
-				condition = "true";
+			class ProcessOil : ProcessTobacco {
+				item = "UnprocessedOil";
+				title = "Process Oil";
+				description = "Process Oil before today's end!";
+			};
+			class ProcessSand : ProcessTobacco {
+				item = "Sand";
+				title = "Process Sand";
+				description = "Process Sand before today's end!";
+			};
+			class ProcessSalt : ProcessTobacco {
+				item = "UnrefinedSalt";
+				title = "Process Salt";
+				description = "Process Salt before today's end!";
+			};
+			class UncutDiamond : ProcessTobacco {
+				item = "Diamond";
+				title = "Process Diamonds";
+				description = "Process Diamond before today's end!";
 			};
 
-			class SellIron {
-				type = "SELLING";
-				item = "IronBar";
-				title = "Iron Suits";
-				description = "Sell Iron Bar(s) before todays end!";
-				tiers[] = { 100, 150, 300 };
+			class ProcessUranium {
+				type = "PROCESSING";
+				item = "UraniumOre";
+				title = "Process Uranium";
+				description = "Process Uranium before today's end!";
+				tiers[] = { 100, 200, 300 };
 				condition = "true";
 			};
 		};
 
 		class Weekly {
 			Amount = 2;
-			
-			class GatherCopper {
-				type = "GATHERING";
-				item = "CopperOre";
-				title = "Copper Gods";
-				description = "Mine Copper Ore before the weeks end!";
-				tiers[] = { 500, 750, 1000 };
-				condition = "true";
-			};
 
-			class GatherSilver {
-				type = "GATHERING";
-				item = "SilverOre";
-				title = "Roads of Silver";
-				description = "Mine Silver Ore before the weeks end!";
-				tiers[] = { 500, 750, 1000 };
-				condition = "true";
-			};
-
-			class SellCopper {
+			class SellSchnapps {
 				type = "SELLING";
-				item = "CopperBar";
-				title = "Throne of Copper";
-				description = "Sell Copper Bar(s) before the weeks end!";
-				tiers[] = { 600, 800, 1000 };
+				item = "Schnapps";
+				title = "Sell Schnapps";
+				description = "Sell Schnapps before the week's end!";
+				tiers[] = { 500, 750, 1000 };
 				condition = "true";
+			};
+			class SellCigarette : SellSchnapps {
+				item = "Cigarette";
+				title = "Sell Cigarettes";
+				description = "Sell Cigarettes before the week's end!";
+			};
+			class SellCigar : SellSchnapps {
+				item = "Cigar";
+				title = "Sell Cigars";
+				description = "Sell Cigars before the week's end!";
+			};
+			class SellCopper : SellSchnapps {
+				item = "CopperBar";
+				title = "Sell Copper";
+				description = "Sell Copper before the week's end!";
+			};
+			class SellSilver : SellSchnapps {
+				item = "SilverBar";
+				title = "Sell Silver";
+				description = "Sell Silver before the week's end!";
+			};
+			class SellIron : SellSchnapps {
+				item = "IronBar";
+				title = "Sell Iron";
+				description = "Sell Iron before the week's end!";
+			};
+			class SellSand : SellSchnapps {
+				item = "Glass";
+				title = "Sell Glass";
+				description = "Sell Sand before the week's end!";
+			};
+			class SellSalt : SellSchnapps {
+				item = "RefindedSalt";
+				title = "Sell Salt";
+				description = "Sell Salt before the week's end!";
+			};
+			class SellDiamond : SellSchnapps {
+				item = "CutDiamond";
+				title = "Sell Diamond";
+				description = "Sell Diamond before the week's end!";
+			};
+			class SellMoonshine : SellSchnapps {
+				item = "Moonshine";
+				title = "Sell Moonshine";
+				description = "Sell Moonshine before the week's end!";
 			};
 		};
 	};

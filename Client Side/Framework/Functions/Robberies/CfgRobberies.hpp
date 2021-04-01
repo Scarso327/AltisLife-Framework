bdbd5d099125bf340752956acd450e1c01d310dd
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-01 13:58:54 +0100

    Added: Supermarket Robbery

--- Client Side/Framework/Functions/Robberies/CfgRobberies.hpp
+++ Client Side/Framework/Functions/Robberies/CfgRobberies.hpp
@@ -9,7 +9,18 @@ class CfgRobberies {
 		reward[] = { 30000, 65000 };
 		leveling[] = { { 5, 15 }, "Robbed Fuel Station", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction < 3";
-		onFail = "hint ""There must be at least 3 officers online to rob this store."";";
+		onFail = "hint ""There must be at least 3 officers online to rob this site."";";
+	};
+
+	class Supermarket {
+		name = "Supermarket";
+		time = 7 * 60;
+		timeout = 5 * 60;
+		factions[] = { "Civilian" };
+		reward[] = { 50000, 95000 };
+		leveling[] = { { 5, 15 }, "Robbed Supermarket", 100 };
+		condition = "[[""Police""]] call ULP_fnc_onlineFaction < 4";
+		onFail = "hint ""There must be at least 4 officers online to rob this site."";";
 	};
 
 	class RegionalBank {
@@ -20,6 +31,6 @@ class CfgRobberies {
 		reward[] = { 140000, 460000 };
 		leveling[] = { { 5, 15 }, "Robbed Fuel Station", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction < 5";
-		onFail = "hint ""There must be at least 5 officers online to rob this store."";";
+		onFail = "hint ""There must be at least 5 officers online to rob this site."";";
 	};
 };

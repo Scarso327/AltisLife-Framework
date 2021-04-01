75243abf56b9897d803ce97141b7ea752c69c786
Jack Farhall <jackfarhall@gmail.com>
2021-04-02 00:46:28 +0100

    Update CfgRandomEvents.hpp

--- Client Side/Framework/Functions/RandomEvents/CfgRandomEvents.hpp
+++ Client Side/Framework/Functions/RandomEvents/CfgRandomEvents.hpp
@@ -1,15 +1,16 @@
 class CfgRandomEvents {
+	/* TESTING SPAWN VALUES
+	SpawnCondition = "true";
+	NotifyDelay = 1 * 60;
+	SpawnDelay = 1 * 60;
+	Cooldown = 2 * 60;
+	RandomAddition = 2 * 60;*/
 	class Airdrop {
-		/*SpawnCondition = "(count ([""Civilian""] call ULP_fnc_allMembers)) >= 20";
+		SpawnCondition = "(count ([""Civilian""] call ULP_fnc_allMembers)) >= 20";
 		NotifyDelay = 10 * 60;
 		SpawnDelay = 30 * 60;
 		Cooldown = 60 * 60;
-		RandomAddition = 20 * 60;*/
-		SpawnCondition = "true";
-		NotifyDelay = 1 * 60;
-		SpawnDelay = 1 * 60;
-		Cooldown = 2 * 60;
-		RandomAddition = 2 * 60;
+		RandomAddition = 20 * 60;
 		class Events {
 			class OnWarning { code = "_this params [[""_message"", """", [""""]]]; hint parseText _message;"; };
 			class OnSpawn { code = "_this params [[""_message"", """", [""""]]]; hint parseText _message;"; };

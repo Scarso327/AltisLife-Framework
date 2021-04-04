4194eca1d0d147e8a2169fab3e7163a13e2cd06e
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-04 17:21:30 +0100

    Update CfgCartels.hpp

--- Client Side/Framework/Functions/Cartels/CfgCartels.hpp
+++ Client Side/Framework/Functions/Cartels/CfgCartels.hpp
@@ -1,45 +1,57 @@
 class CfgCartels {
 	class Fixed {
 		cartelTick = 1 * 60;
-		rewardTicks = 15; // Give reward every 15 ticks... (cartelTick * 15) seconds
+		rewardTicks = 15;
 
 		class Northern {
 			name = "Northern Drug Cartel";
 			factions[] = { "Police", "Civilian" };
-			position[] = {14289, 13030.1, 0};
+			position[] = {14295, 13029, 0};
 			zoneRadius = 50;
 			kosRadius = 300;
 			class Drug {
-				extraPay = 0.15;
+				extraPay = 0.2;
 				saleTax = 0.1;
+			};
+		};
+
+		class Southern {
+			name = "Southern Drug Cartel";
+			factions[] = { "Police", "Civilian" };
+			position[] = {11194, 8717, 0};
+			zoneRadius = 75;
+			kosRadius = 0;
+			class Drug {
+				extraPay = 0.1;
+				saleTax = 0.2;
 			};
 		};
 
 		class Church {
 			name = "Church Arms Cartel";
 			factions[] = { "Police", "Civilian" };
-			position[] = {8916.57, 7482.06, 0};
-			zoneRadius = 100;
-			kosRadius = 1000;
+			position[] = {8926, 74936, 0};
+			zoneRadius = 50;
+			kosRadius = 0;
 			class Arms {
-				primaryDiscount = 0.1;
-				secondaryDiscount = 0.15;
+				primaryDiscount = 0.25;
+				secondaryDiscount = 0.25;
 			};
 		};
 
 		class Zaros {
 			name = "Zaros Bay Cartel";
 			factions[] = { "Police", "Civilian" };
-			position[] = {10027.1, 10125.1, 0};
-			zoneRadius = 80;
-			kosRadius = 100;
+			position[] = {10037, 10130, 0};
+			zoneRadius = 40;
+			kosRadius = 0;
 			class Drug {
-				extraPay = 0.15;
+				extraPay = 0.1;
 				saleTax = 0.1;
 			};
 			class Arms {
 				primaryDiscount = 0.1;
-				secondaryDiscount = 0.15;
+				secondaryDiscount = 0.1;
 			};
 		};
 	};

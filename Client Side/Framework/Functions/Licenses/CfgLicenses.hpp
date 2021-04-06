ba5d434bc5bec539c107e8eae83adaa9a45c3941
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-06 12:24:07 +0100

    Licence Pricing

--- Client Side/Framework/Functions/Licenses/CfgLicenses.hpp
+++ Client Side/Framework/Functions/Licenses/CfgLicenses.hpp
@@ -10,7 +10,7 @@ class CfgLicenses {
 
     class Driving : BaseLicense {
         displayName = "Driving Licence";
-        buyPrice = 0;
+        buyPrice = 1000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -19,7 +19,7 @@ class CfgLicenses {
 
     class Trucking : BaseLicense {
         displayName = "Trucking Licence";
-        buyPrice = 0;
+        buyPrice = 60000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -28,7 +28,7 @@ class CfgLicenses {
 
     class Boating : BaseLicense {
         displayName = "Boating Licence";
-        buyPrice = 0;
+        buyPrice = 6500;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -37,7 +37,7 @@ class CfgLicenses {
 
     class Piloting : BaseLicense {
         displayName = "Piloting Licence";
-        buyPrice = 0;
+        buyPrice = 320000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -46,7 +46,7 @@ class CfgLicenses {
 
     class Home : BaseLicense {
         displayName = "Homeowner's Approval";
-        buyPrice = 0;
+        buyPrice = 3000000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -55,7 +55,7 @@ class CfgLicenses {
 
     class Taxi : BaseLicense {
         displayName = "Taxi Altis Worker";
-        buyPrice = 0;
+        buyPrice = 35000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -64,7 +64,7 @@ class CfgLicenses {
 
     class Solicitor : BaseLicense {
         displayName = "Altis Solicitors";
-        buyPrice = 0;
+        buyPrice = 1700000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -73,7 +73,7 @@ class CfgLicenses {
 
     class News : BaseLicense {
         displayName = "AAN World News Reporter";
-        buyPrice = 0;
+        buyPrice = 700000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -91,7 +91,7 @@ class CfgLicenses {
 
     class Handgun : BaseLicense {
         displayName = "Handgun Certificate";
-        buyPrice = 0;
+        buyPrice = 65000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -100,7 +100,7 @@ class CfgLicenses {
 
     class Hunting : BaseLicense {
         displayName = "Hunting Certificate";
-        buyPrice = 0;
+        buyPrice = 14000;
         class Settings {
             isVisible = true;
             isRemovable = true;
@@ -109,104 +109,104 @@ class CfgLicenses {
 
     class Rebel : BaseLicense {
         displayName = "Rebel Access";
-        buyPrice = 0;
+        buyPrice = 5000000;
         class Settings {};
     };
 
     class Blackmarket : BaseLicense {
         displayName = "Blackmarket Access";
-        buyPrice = 0;
+        buyPrice = 15000000;
         class Settings {};
     };
 
     // Processing Sites
     class Schnapps : BaseLicense {
         displayName = "Schnapps Processing";
-        buyPrice = 0;
+        buyPrice = 15000;
         class Settings {};
     };
 
     class Tobacco : BaseLicense {
         displayName = "Tobacco Processing";
-        buyPrice = 0;
+        buyPrice = 45000;
         class Settings {};
     };
 
     class Copper : BaseLicense {
         displayName = "Copper Processing";
-        buyPrice = 0;
+        buyPrice = 30000;
         class Settings {};
     };
 
     class Silver : BaseLicense {
         displayName = "Silver Processing";
-        buyPrice = 0;
+        buyPrice = 35000;
         class Settings {};
     };
 
     class Iron : BaseLicense {
         displayName = "Iron Processing";
-        buyPrice = 0;
+        buyPrice = 290000;
         class Settings {};
     };
 
     class Salt : BaseLicense {
         displayName = "Salt Processing";
-        buyPrice = 0;
+        buyPrice = 140000;
         class Settings {};
     };
 
     class Sand : BaseLicense {
         displayName = "Sand Processing";
-        buyPrice = 0;
+        buyPrice = 155000;
         class Settings {};
     };
 
     class Oil : BaseLicense {
         displayName = "Oil Processing";
-        buyPrice = 0;
+        buyPrice = 265000;
         class Settings {};
     };
 
     class Diamond : BaseLicense {
         displayName = "Diamond Processing";
-        buyPrice = 0;
+        buyPrice = 310000;
         class Settings {};
     };
 
     class Uranium : BaseLicense {
         displayName = "Uranium Processing";
-        buyPrice = 0;
+        buyPrice = 800000;
         class Settings {};
     };
 
     class Weed : BaseLicense {
         displayName = "Weed Processing";
-        buyPrice = 0;
+        buyPrice = 80000;
         class Settings {};
     };
 
     class Heroin : BaseLicense {
         displayName = "Heroin Processing";
-        buyPrice = 0;
+        buyPrice = 195000;
         class Settings {};
     };
 
     class Cocaine : BaseLicense {
         displayName = "Cocaine Processing";
-        buyPrice = 0;
+        buyPrice = 710000;
         class Settings {};
     };
 
     class Meth : BaseLicense {
         displayName = "Meth Processing";
-        buyPrice = 0;
+        buyPrice = 785000;
         class Settings {};
     };
 
     class Moonshine : BaseLicense {
         displayName = "Moonshine Processing";
-        buyPrice = 0;
+        buyPrice = 1050000;
         class Settings {};
     };
 };

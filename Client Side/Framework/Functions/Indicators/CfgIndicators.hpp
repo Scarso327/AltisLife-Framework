340e1a67a698503c32c292965376c4145f663685
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-11 22:59:27 +0100

    Added: Indicator Colours

--- Client Side/Framework/Functions/Indicators/CfgIndicators.hpp
+++ Client Side/Framework/Functions/Indicators/CfgIndicators.hpp
@@ -1,12 +1,27 @@
 class CfgIndicators {
-	groupColour[] = { 1, 1, 1, 1 };
-	alignedColour[] = { 1, 1, 1, 1 };
+	groupColour[] = { 0.502, 0.502, 0.502, 1 };
+	alignedColour[] = { 0, 0.502, 0.502, 1 };
 
 	class Police {
+		// NPAS
+		class U_O_PilotCoveralls { colour[] = { 1, 0.647, 0, 1 }; };
+
 		// SCO19
 		class U_B_CTRG_Soldier_F { colour[] = { 0.8, 0, 0, 1 }; };
 		class U_B_GhillieSuit : U_B_CTRG_Soldier_F {};
+
+		// NCA
+		class U_O_R_Gorka_01_black_F { colour[] = { 0, 0.502, 0, 1 }; };
+
+		// MPU
+		class U_B_Wetsuit { colour[] = { 0, 0, 1, 1 }; };
+
+		// RPU
+		class U_B_CombatUniform_mcam_worn { colour[] = { 1, 1, 0, 1 }; };
 	};
 
-	class Medic {};
+	class Medic {
+		// AAA
+		class U_O_PilotCoveralls { colour[] = { 0.8, 0, 0, 1 }; };
+	};
 };

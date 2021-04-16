4fdc45000de8996a672ab2528dd37f6815a3dfb2
Jack Farhall <jackfarhall@gmail.com>
2021-04-16 03:33:47 +0100

    Updated : Select UI

--- Client Side/Framework/UI/DisplaySelect/DisplaySelectNumber.hpp
+++ Client Side/Framework/UI/DisplaySelect/DisplaySelectNumber.hpp
@@ -1,6 +1,7 @@
-#define UI_X 0.29375
-#define UI_WIDTH 0.19
-#define BODY_HEIGHT 0.066
+#define UI_WIDTH 0.175
+#define UI_X 0.5 - (UI_WIDTH / 2)
+#define BODY_HEIGHT 0.1
+#define BODY_Y 0.5 - (BODY_HEIGHT / 2)
 
 class DisplaySelectNumber {
     idd = 3200;
@@ -31,64 +32,67 @@ class DisplaySelectNumber {
 			idc = 3201;
 			colorBackground[] = HEADER_COLOUR;
 			text = "Select Amount";
-			SAFEZONE_X(UI_X + 0.1125);
-			SAFEZONE_Y(0.434);
+			SAFEZONE_X(UI_X);
+			SAFEZONE_Y(BODY_Y - 0.022);
 			SAFEZONE_W(UI_WIDTH);
 			SAFEZONE_H(0.022);
 		};
 
 		class Body : Life_RscText {
-			idc = 3202;
+			idc = -1;
 			colorBackground[] = BODY_COLOUR;
-			SAFEZONE_X(UI_X + 0.1125);
-			SAFEZONE_Y(0.456);
+			SAFEZONE_X(UI_X);
+			SAFEZONE_Y(BODY_Y);
 			SAFEZONE_W(UI_WIDTH);
 			SAFEZONE_H(BODY_HEIGHT);
 		};
 
 		class Footer : Life_RscText {
-			idc = 3203;
+			idc = -1;
 			colorBackground[] = FOOTER_COLOUR;
-			SAFEZONE_X(UI_X + 0.1125);
-			SAFEZONE_Y(0.52);
+			SAFEZONE_X(UI_X);
+			SAFEZONE_Y((BODY_Y + BODY_HEIGHT));
 			SAFEZONE_W(UI_WIDTH);
 			SAFEZONE_H(0.033);
 		};
 	};
 
 	class Controls {
+
+		#define ITEM_HEIGHTS VERTICAL_GRID_H((BODY_HEIGHT - (MARGIN_Y * 2)), 3, (MARGIN_Y / 2))
+
 		class MinMaxText : Life_RscStructuredText {
 			idc = 3204;
-			SAFEZONE_X((UI_X + 0.1125) + MARGIN_X);
-			SAFEZONE_Y(0.456 + MARGIN_Y);
+			SAFEZONE_X(UI_X + MARGIN_X);
+			SAFEZONE_Y(VERTICAL_GRID_Y((BODY_Y + MARGIN_Y), ITEM_HEIGHTS, 0, (MARGIN_Y / 2)));
 			SAFEZONE_W(UI_WIDTH - (MARGIN_X * 2));
-			SAFEZONE_H(0.022);
+			SAFEZONE_H(ITEM_HEIGHTS);
 		};
 
 		class ValueInput : life_RscEdit {
 			idc = 3209;
 			text = "";
 			colorDisabled[] = {0.95, 0.95, 0.95, 1};
-			SAFEZONE_X((UI_X + 0.1125) + MARGIN_X);
-			SAFEZONE_Y(0.456 + MARGIN_Y);
+			SAFEZONE_X(UI_X + MARGIN_X);
+			SAFEZONE_Y(VERTICAL_GRID_Y((BODY_Y + MARGIN_Y), ITEM_HEIGHTS, 1, (MARGIN_Y / 2)));
 			SAFEZONE_W(UI_WIDTH - (MARGIN_X * 2));
-			SAFEZONE_H(0.022);
+			SAFEZONE_H(ITEM_HEIGHTS);
 		};
 
 		class Slider : life_RscXSliderH {
 			idc = 3205;
-			SAFEZONE_X((UI_X + 0.1125) + MARGIN_X);
-			SAFEZONE_Y((0.456 + 0.022) + MARGIN_Y);
+			SAFEZONE_X(UI_X + MARGIN_X);
+			SAFEZONE_Y(VERTICAL_GRID_Y((BODY_Y + MARGIN_Y), ITEM_HEIGHTS, 2, (MARGIN_Y / 2)));
 			SAFEZONE_W(UI_WIDTH - (MARGIN_X * 2));
-			SAFEZONE_H(0.022);
+			SAFEZONE_H(ITEM_HEIGHTS);
 		};
 
 		class Select : Life_RscButtonCenter {
 			idc = 3206;
 			text = "<t align = 'center'>Select</t>";
 			onButtonClick = "(ctrlParent (_this select 0)) closeDisplay 1;";
-			SAFEZONE_X(((UI_X + 0.1125) + UI_WIDTH - (UI_WIDTH / 4)) - MARGIN_X);
-			SAFEZONE_Y(0.52 + BUTTON_MARGIN_Y);
+			SAFEZONE_X((UI_X + UI_WIDTH - (UI_WIDTH / 4)) - MARGIN_X);
+			SAFEZONE_Y((BODY_Y + BODY_HEIGHT) + BUTTON_MARGIN_Y);
 			SAFEZONE_W((UI_WIDTH / 4));
 			SAFEZONE_H(0.022);
 		};

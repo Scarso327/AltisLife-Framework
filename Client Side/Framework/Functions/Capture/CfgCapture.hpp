c1829f9f9e040b8e8af189726334020456d9d98b
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-17 01:13:29 +0100

    Added: Criminal Hideouts

--- Client Side/Framework/Functions/Capture/CfgCapture.hpp
+++ Client Side/Framework/Functions/Capture/CfgCapture.hpp
@@ -1,9 +1,9 @@
 class CfgCapture {
 	class Altis {
 		class Hideout {
-			displayName = "Hideout";
+			displayName = "Criminal Hideout";
 			time = 180;
-			condition = "true";
+			condition = "[""Hideout""] call ULP_fnc_hasGroupPerk";
 		};
 	};
 };

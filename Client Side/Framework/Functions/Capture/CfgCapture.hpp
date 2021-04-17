b767f70ae04082941f1b346d32b95063084758fa
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-17 17:36:36 +0100

    Added: Difference in Hideouts

--- Client Side/Framework/Functions/Capture/CfgCapture.hpp
+++ Client Side/Framework/Functions/Capture/CfgCapture.hpp
@@ -1,9 +1,10 @@
 class CfgCapture {
 	class Altis {
-		class Hideout {
-			displayName = "Criminal Hideout";
+		class WestHideout {
+			displayName = "Western Criminal Hideout";
 			time = 180;
 			condition = "[""Hideout""] call ULP_fnc_hasGroupPerk";
 		};
+		class EastHideout : WestHideout { displayName = "Eastern Criminal Hideout"; };
 	};
 };

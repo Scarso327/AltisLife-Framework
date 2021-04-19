d4dd61e27d43d49346ef050c087832953c3c71cc
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-19 17:31:05 +0100

    Added: Icon to Debriefing Screen

--- Client Side/Framework/CfgDebriefing.hpp
+++ Client Side/Framework/CfgDebriefing.hpp
@@ -1,28 +1,20 @@
 class CfgDebriefing {
     class Abort {
-        title = "Aborted";
+        title = "Left the Game";
         subtitle = "Thanks For Playing!";
         description = "Your data has been synced and you've returned to the lobby.";
         pictureBackground = "";
-        picture = "";
+        picture = "\A3\ui_f\data\GUI\Cfg\Hints\Tasks_ca.paa";
         pictureColor[] = {0,0.3,0.6,1};
     };
-
-    class NotWhitelisted {
+    class NotWhitelisted : Abort {
         title = "Unable to Join";
         subtitle = "You are not currently whitelisted";
         description = "Please contact a member of this faction's command for more information";
-        pictureBackground = "";
-        picture = "";
-        pictureColor[] = {0,0.3,0.6,1};
     };
-
     class Blacklisted {
         title = "Unable to Join";
         subtitle = "You are currently blacklisted";
         description = "Please contact a member of this faction's command for more information";
-        pictureBackground = "";
-        picture = "";
-        pictureColor[] = {0,0.3,0.6,1};
     };
 };

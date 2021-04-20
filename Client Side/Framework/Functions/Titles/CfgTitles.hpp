481cfe2c265d19071ded3a8bc51f218c31cb2f8f
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-20 21:23:06 +0100

    Added: Police and Crime Commissioner Title

--- Client Side/Framework/Functions/Titles/CfgTitles.hpp
+++ Client Side/Framework/Functions/Titles/CfgTitles.hpp
@@ -99,4 +99,8 @@ class CfgTitles {
 		displayName = "Ocean Diver";
 		description = "Dived to a very specific place";
 	};
+	class PoliceCrimeCommissioner {
+		displayName = "Police and Crime Commissioner";
+		description = "daddy";
+	};
 };

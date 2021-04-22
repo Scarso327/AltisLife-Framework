3ca27665a5de79a30ba3ec013fb8698b66a425e4
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-04-22 16:23:16 +0100

    Added: Location Name to Cameras

--- Client Side/Framework/Functions/Camera/CfgCameras.hpp
+++ Client Side/Framework/Functions/Camera/CfgCameras.hpp
@@ -4,22 +4,22 @@ class CfgCameras {
 			condition = "true";
 			class Cameras {
 				class CarPark {
-					name = "HMP Car Park";
+					name = "HM Prison Car Park";
 					position[] = {12337.519,14376.288,4.363};
 					target[] = {12351.6,14368.9,0};
 				};
 				class Lobby {
-					name = "HMP Lobby";
+					name = "HM Prison Lobby";
 					position[] = {12328.3,14372.3,4.43624};
 					target[] = {12338,14369.9,0};
 				};
 				class MainHall {
-					name = "HMP Main Hall";
+					name = "HM Prison Main Hall";
 					position[] = {12341.158,14366.455,4.105};
 					target[] = {12336.4,14345.1,0};
 				};
 				class Yard {
-					name = "HMP Yard";
+					name = "HM Prison Yard";
 					position[] = {12333.9,14337.6,5.12775};
 					target[] = {12324.5,14345.4,0};
 				};
@@ -30,17 +30,17 @@ class CfgCameras {
 			condition = "true";
 			class Cameras {
 				class WestEntrance {
-					name = "West Entrance";
+					name = "HM Treasury West Entrance";
 					position[] = {16073,16977.8,12.2223};
 					target[] = {15953.3,16996.2,0};
 				};
 				class EastEntrance {
-					name = "East Entrance";
+					name = "HM Treasury East Entrance";
 					position[] = {16107.4,17010.8,12.2263};
 					target[] = {16191.9,17002.9,0};
 				};
 				class FreightEntrance {
-					name = "Freight Entrance";
+					name = "HM Treasury Freight Entrance";
 					position[] = {16044.3,16871.2,12.8568};
 					target[] = {16105.3,16848.5,0};
 				};
@@ -51,27 +51,27 @@ class CfgCameras {
 			condition = "true";
 			class Cameras {
 				class ForwardDeck {
-					name = "Forward Deck";
+					name = "HMS Liberty Forward Deck";
 					position[] = {14051.4,11509.1,26.7665};
 					target[] = {14050.8,11541.9,0};
 				};
 				class LandingDeck {
-					name = "Landing Deck";
+					name = "HMS Liberty Landing Deck";
 					position[] = {14058,11404.9,51.6275};
 					target[] = {14059.3,11390,0};
 				};
 				class MainHangar {
-					name = "Main Hangar";
+					name = "HMS Liberty Main Hangar";
 					position[] = {14059.2,11440.9,33.7622};
 					target[] = {14059.6,11420.2,0};
 				};
 				class MainHallway {
-					name = "Main Hallway";
+					name = "HMS Liberty Main Hallway";
 					position[] = {14053.4,11478.8,19.9895};
 					target[] = {14056.8,11463.7,0};
 				};
 				class CommandDeck {
-					name = "Command Deck";
+					name = "HMS Liberty Command Deck";
 					position[] = {14045.3,11499.8,28.8083};
 					target[] = {14052.9,11502.3,0};
 				};
@@ -82,17 +82,17 @@ class CfgCameras {
 			condition = "true";
 			class Cameras {
 				class Entrance {
-					name = "Site Entrance";
+					name = "Evidence Storage Site Entrance";
 					position[] = {3248.53,12451.2,5.08133};
 					target[] = {3232.38,12469,0};
 				};
 				class NorthBuilding {
-					name = "North Building";
+					name = "Evidence Storage North Building";
 					position[] = {3248.09,12474.2,4.63247};
 					target[] = {3266.84,12467,0};
 				};
 				class SouthBuilding {
-					name = "HMP Main Hall";
+					name = "Evidence Storage Main Hall";
 					position[] = {3286,12441.7,4.30197};
 					target[] = {3265.56,12449.4,0};
 				};

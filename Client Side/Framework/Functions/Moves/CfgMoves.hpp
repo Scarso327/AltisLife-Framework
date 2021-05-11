dc0f8bcc1e2f73f0e57cee194cf0c59185f65879
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2021-05-12 00:50:24 +0100

    Added: More Moves (Tested Individually)

--- Client Side/Framework/Functions/Moves/CfgMoves.hpp
+++ Client Side/Framework/Functions/Moves/CfgMoves.hpp
@@ -1,61 +1,124 @@
 class CfgMoves {
-	 class KungFu {
-        displayName = "Kung Fu";
-        animation = "AmovPercMstpSnonWnonDnon_exerciseKata";
+    class Wave1 {
+        displayName = "Wave #1";
+        animation = "HudWave_move1";
     };
-    
-    class PushUps {
-        displayName = "Push Ups";
-        animation = "AmovPercMstpSnonWnonDnon_exercisePushup";
-    };
-    
-    class KneeBendsFast {
-        displayName = "Knee Bends (Fast)";
-        animation = "AmovPercMstpSnonWnonDnon_exercisekneeBendB";
-    };
-    
-    class KneeBendsNormal {
-        displayName = "Knee Bends (Normal)";
-        animation = "AmovPercMstpSnonWnonDnon_exercisekneeBendA";
+    class Wave2 {
+        displayName = "Wave #2";
+        animation = "HudWave_move2";
     };
 
-    class Approval {
-        displayName = "Approval";
-        animation = "Acts_ambient_agreeing";
+    class Aggressive {
+        displayName = "Act Aggressive";
+        animation = "Acts_Ambient_Aggressive";
     };
-    
-    class Disapproval {
-        displayName = "Disapproval";
-        animation = "Acts_ambient_disagreeing";
+    class Defensive {
+        displayName = "Act Defensive";
+        animation = "Acts_Ambient_Defensive";
     };
 
-    class Facepalm {
-        displayName = "Facepalm";
-        animation = "Acts_ambient_facepalm_2";
+    class Agreeing {
+        displayName = "Agree";
+        animation = "Acts_Ambient_Agreeing";
+    };
+    class Disagreeing {
+        displayName = "Disagree";
+        animation = "Acts_Ambient_Disagreeing";
+    };
+    class DisagreeingPoint {
+        displayName = "Disagree and Point";
+        animation = "Acts_Ambient_Disagreeing_with_pointing";
+    };
+    class Dismissing {
+        displayName = "Dismiss";
+        animation = "Acts_Ambient_Mismissing";
+    };
+
+    class Approximate {
+        displayName = "Approximate";
+        animation = "Acts_Ambient_Approximate";
+    };
+    class Huh {
+        displayName = "Huh?";
+        animation = "Acts_Ambient_Huh";
+    };
+
+    class TieShoelaces {
+        displayName = "Tie Shoelaces";
+        animation = "Acts_Ambient_Shoelaces";
+    };
+
+    class Yawn {
+        displayName = "Yawn";
+        animation = "Acts_Ambient_Gestures_Yawn";
+    };
+    class Stretch {
+        displayName = "Stretch";
+        animation = "Acts_Ambient_Stretching";
+    };
+    class Tired {
+        displayName = "Act Tired";
+        animation = "Acts_Ambient_Gestures_Tired";
+    };
+
+    class Sneeze {
+        displayName = "Sneeze";
+        animation = "Acts_Ambient_Gestures_Sneeze";
+    };
+    class CleanNose {
+        displayName = "Clean Nose";
+        animation = "Acts_Ambient_Cleaning_Nose";
+    };
+
+    class Explain {
+        displayName = "Explain";
+        animation = "Act_Alien_Gesture";
+    };
+
+    class SlowFacepalm {
+        displayName = "Slow Facepalm";
+        animation = "Acts_Ambient_Facepalm_1";
+    };
+    class QuickFacepalm {
+        displayName = "Quick Facepalm";
+        animation = "Acts_Ambient_Facepalm_2";
+    };
+
+    class Shocked {
+        displayName = "Act Shocked";
+        animation = "Acts_ShockedUnarmed_2_Loop";
+    };
+
+    class Grieve {
+        displayName = "Grieve";
+        animation = "Acts_Grieving";
     };
 
     class Scared {
-        displayName = "Scared";
-        animation = "AmovPercMstpSnonWnonDnon_Scared";
+        displayName = "Act Scared";
+        animation = "Acts_CivilHiding_2";
     };
 
-    class Throw {
-        displayName = "Throw";
-        animation = "AwopPercMstpSgthWpstDnon_Part5";
+    class Dance1 {
+        displayName = "Dance #1";
+        animation = "Acts_Dance_01";
+    };
+    class Dance2 {
+        displayName = "Dance #2";
+        animation = "Acts_Dance_02";
     };
 
-    class Slap {
-        displayName = "Slap";
-        animation = "AwopPercMstpSgthWrflDnon_Start1";
+    class KungFu {
+        displayName = "Do Kung Fu";
+        animation = "AmovPercMstpSnonWnonDnon_exerciseKata";
+    };
+    class PushUps {
+        displayName = "Do Push Ups";
+        animation = "AmovPercMstpSnonWnonDnon_exercisePushup";
     };
 
-    class Hoodini {
-        displayName = "Hoodini";
-        animation = "AswmPercMstpSnonWnonDnon";
-    };
-
-    class Pissing {
-        displayName = "Pissing";
+    class Piss {
+        displayName = "Piss";
         animation = "Acts_AidlPercMstpSlowWrflDnon_pissing";
     };
 };

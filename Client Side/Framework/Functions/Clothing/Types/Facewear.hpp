63b1306a50091f5edbd9a457470a94dd51d30f0d
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2023-11-06 21:44:38 +0000

    Updated clothing store with new textures

--- Client Side/Framework/Functions/Clothing/Types/Facewear.hpp
+++ Client Side/Framework/Functions/Clothing/Types/Facewear.hpp
@@ -226,16 +226,36 @@ class G_Bandanna_aviator {
 			condition = "[] call ULP_fnc_donatorLevel > 0";  
 		};
 		class G_Bandanna_blk : G_Bandanna_aviator { displayName = "Black"; };
-		class G_Bandanna_khk : G_Bandanna_aviator{ displayName = "Khaki"; };
-		class G_Bandanna_oli : G_Bandanna_aviator{ displayName = "Olive"; };
+		class G_Bandanna_khk : G_Bandanna_aviator { displayName = "Khaki"; };
+		class G_Bandanna_oli : G_Bandanna_aviator { displayName = "Olive"; };
+		class G_Bandanna_BlueFlame1 : G_Bandanna_beast { displayName = "Blue Flame"; };
+		class G_Bandanna_BlueFlame2 : G_Bandanna_beast { displayName = "Blue Flames"; };
+		class G_Bandanna_CandySkull : G_Bandanna_beast { displayName = "Candy Skull"; };
+		class G_Bandanna_OrangeFlame1 : G_Bandanna_beast { displayName = "Orange Flames"; };
+		class G_Bandanna_RedFlame1 : G_Bandanna_beast { displayName = "Red Flames"; };
+		class G_Bandanna_Skull1 : G_Bandanna_beast { displayName = "Skull"; };
+		class G_Bandanna_Skull2 : G_Bandanna_beast { displayName = "Skull (Ornamental)"; };
+		class G_Bandanna_Syndikat1 : G_Bandanna_beast { displayName = "Syndikat"; };
+		class G_Bandanna_Syndikat2 : G_Bandanna_beast { displayName = "Syndikat"; };
+		class G_Bandanna_Vampire_01 : G_Bandanna_beast { displayName = "Vampire"; };
 	};
 };
-class G_Bandanna_shades : G_Bandanna_aviator{};
-class G_Bandanna_sport : G_Bandanna_aviator{};
-class G_Bandanna_beast : G_Bandanna_aviator{};
-class G_Bandanna_blk : G_Bandanna_aviator{};
-class G_Bandanna_khk : G_Bandanna_aviator{};
-class G_Bandanna_oli : G_Bandanna_aviator{};
+class G_Bandanna_shades : G_Bandanna_aviator {};
+class G_Bandanna_sport : G_Bandanna_aviator {};
+class G_Bandanna_beast : G_Bandanna_aviator {};
+class G_Bandanna_blk : G_Bandanna_aviator {};
+class G_Bandanna_khk : G_Bandanna_aviator {};
+class G_Bandanna_oli : G_Bandanna_aviator {};
+class G_Bandanna_BlueFlame1 : G_Bandanna_aviator {};
+class G_Bandanna_BlueFlame2 : G_Bandanna_aviator {};
+class G_Bandanna_CandySkull : G_Bandanna_aviator {};
+class G_Bandanna_OrangeFlame1 : G_Bandanna_aviator {};
+class G_Bandanna_RedFlame1 : G_Bandanna_aviator {};
+class G_Bandanna_Skull1 : G_Bandanna_aviator {};
+class G_Bandanna_Skull2 : G_Bandanna_aviator {};
+class G_Bandanna_Syndikat1 : G_Bandanna_aviator {};
+class G_Bandanna_Syndikat2 : G_Bandanna_aviator {};
+class G_Bandanna_Vampire_01 : G_Bandanna_aviator {};
 
 class G_Balaclava_blk {
 	displayName = "Balaclava";
@@ -249,14 +269,31 @@ class G_Balaclava_blk {
 			displayName = "Black";
 			condition = "true";
 		};
-		class G_Balaclava_lowprofile : G_Balaclava_blk{ displayName = "Black + Lowprofile Goggles"; };
-		class G_Balaclava_combat : G_Balaclava_blk{ displayName = "Black + Combat Goggles"; };
-		class G_Balaclava_oli : G_Balaclava_blk{ displayName = "Olive"; };
+		class G_Balaclava_lowprofile : G_Balaclava_blk { displayName = "Black + Lowprofile Goggles"; };
+		class G_Balaclava_combat : G_Balaclava_blk { displayName = "Black + Combat Goggles"; };
+		class G_Balaclava_oli : G_Balaclava_blk { displayName = "Olive"; };
+		class G_Balaclava_BlueStrips : G_Balaclava_blk { 
+			displayName = "Blue Stripe"; 
+			condition = "[] call ULP_fnc_donatorLevel > 0";
+		};
+		class G_Balaclava_Flecktarn : G_Balaclava_BlueStrips { displayName = "Flecktarn"; };
+		class G_Balaclava_Halloween_01 : G_Balaclava_BlueStrips { displayName = "Halloween"; };
+		class G_Balaclava_Flames1 : G_Balaclava_BlueStrips { displayName = "Orange Pattern"; };
+		class G_Balaclava_Scarecrow_01 : G_Balaclava_BlueStrips { displayName = "Scarecrow"; };
+		class G_Balaclava_Skull1 : G_Balaclava_BlueStrips { displayName = "Skull"; };
+		class G_Balaclava_Tropentarn : G_Balaclava_BlueStrips { displayName = "Tropentarn"; };
 	};
 };
-class G_Balaclava_lowprofile : G_Balaclava_blk{};
-class G_Balaclava_combat : G_Balaclava_blk{};
-class G_Balaclava_oli : G_Balaclava_blk{};
+class G_Balaclava_lowprofile : G_Balaclava_blk {};
+class G_Balaclava_combat : G_Balaclava_blk {};
+class G_Balaclava_oli : G_Balaclava_blk {};
+class G_Balaclava_BlueStrips : G_Balaclava_blk {};
+class G_Balaclava_Flecktarn : G_Balaclava_blk {};
+class G_Balaclava_Halloween_01 : G_Balaclava_blk {};
+class G_Balaclava_Flames1 : G_Balaclava_blk {};
+class G_Balaclava_Scarecrow_01 : G_Balaclava_blk {};
+class G_Balaclava_Skull1 : G_Balaclava_blk {};
+class G_Balaclava_Tropentarn : G_Balaclava_blk {};
 
 class G_Balaclava_TI_blk_F {
 	displayName = "Stealth Balaclava";
@@ -270,7 +307,10 @@ class G_Balaclava_TI_blk_F {
 			displayName = "Black";
 			condition = "true";
 		};
-		class G_Balaclava_TI_G_blk_F : G_Balaclava_TI_blk_F { displayName = "Black + Goggles"; };
+		class G_Balaclava_TI_G_blk_F : G_Balaclava_TI_blk_F { 
+			displayName = "Black + Goggles"; 
+			isNV = true;
+		};
 	};
 };
-class G_Balaclava_TI_G_blk_F : G_Balaclava_TI_blk_F{};
+class G_Balaclava_TI_G_blk_F : G_Balaclava_TI_blk_F {};

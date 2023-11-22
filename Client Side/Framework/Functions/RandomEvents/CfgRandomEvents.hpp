90c0c870f20e325397d50b78d32b4b7fc1f36238
Jack Farhall <jackfarhall@gmail.com>
2023-11-22 22:04:23 +0000

    Changed: Airdrop times

--- Client Side/Framework/Functions/RandomEvents/CfgRandomEvents.hpp
+++ Client Side/Framework/Functions/RandomEvents/CfgRandomEvents.hpp
@@ -10,13 +10,13 @@ class CfgRandomEvents {
 	class Airdrop {
 		SpawnCondition = "(count ([""Civilian""] call ULP_fnc_allMembers)) >= 20";
 		NotifyDelay = 10 * 60;
-		Cooldown = 60 * 60;
+		Cooldown = 40 * 60;
 		RandomAddition = 20 * 60;
 		class BaseWars {
 			SpawnCondition = "(count ([""Civilian""] call ULP_fnc_allMembers)) >= 10";
 			NotifyDelay = 5 * 60;
 			Cooldown = 20 * 60;
-			RandomAddition = 2 * 60;
+			RandomAddition = 5 * 60;
 		};
 		class Debug {
 			SpawnCondition = "true";

04a756474df3fd489d7222f7edab7fc5aa6af8e7
Jack Farhall <jackfarhall@gmail.com>
2023-11-25 21:59:26 +0000

    Revert "#318 - Inventory searching"
    This reverts commit 560b94874fb9420cd9c7370d832fea31135834da.

--- Client Side/Framework/Functions/VirtualInventory/fn_updateInventory.sqf
+++ Client Side/Framework/Functions/VirtualInventory/fn_updateInventory.sqf
@@ -19,20 +19,23 @@ if (isNull _display) exitWith { false };
 private _container = _display getVariable ["container", objNull];
 if (isNull _container) exitWith { false };
 
-private _addItem = {
-	_this params [ "_list", "_searchValue", "_x", "_y" ];
+if (_type isEqualTo 0) then {
+	ULP_CarryInfo params ["_carryWeight", "_maxWeight"];
 
-	private _cfg = missionConfigFile >> "CfgVirtualItems" >> _x;
+	private _personalTitle = _display displayCtrl 4202;
+	_personalTitle ctrlSetStructuredText parseText format["Personal Inventory<t align='right'>%1/%2</t>", _carryWeight, _maxWeight];
 
-	if (isClass _cfg) then {
-		private _itemIcon = getText (_cfg >> "icon");
+	private _list = _display displayCtrl 4204;
+	lnbClear _list;
 
-		if ([getNumber (_cfg >> "Settings" >> "isScripted")] call ULP_fnc_bool) then {
-			{
-				private _displayName = format [getText (_cfg >> "displayName"), _x];
+	{
+		private _cfg = missionConfigFile >> "CfgVirtualItems" >> _x;
+		if (isClass _cfg) then {
+			private _itemIcon = getText (_cfg >> "icon");
 
-				if (((toLower _displayName) find _searchValue) > -1) then {
-					private _row = _list lnbAddRow [_displayName, "-"];
+			if ([getNumber (_cfg >> "Settings" >> "isScripted")] call ULP_fnc_bool) then {
+				{
+					private _row = _list lnbAddRow [format [getText (_cfg >> "displayName"), _x], "-"];
 
 					if !(_itemIcon isEqualTo "") then {
 						_list lnbSetPicture [[_row, 0], _itemIcon];
@@ -40,12 +43,8 @@ private _addItem = {
 
 					_list lnbSetData [[_row, 0], configName _cfg];
 					_list lnbSetData [[_row, 1], _x];
-				};
-			} forEach _y;
-		} else {
-			private _displayName = getText (_cfg >> "displayName");
-
-			if (((toLower _displayName) find _searchValue) > -1) then {
+				} forEach _y;
+			} else {
 				private _row = _list lnbAddRow [
 					getText (_cfg >> "displayName"), 
 					[_y] call ULP_fnc_numberText
@@ -59,22 +58,6 @@ private _addItem = {
 				_list lnbSetValue [[_row, 1], _y];
 			};
 		};
-	};
-};
-
-if (_type isEqualTo 0) then {
-	ULP_CarryInfo params ["_carryWeight", "_maxWeight"];
-
-	private _personalTitle = _display displayCtrl 4202;
-	_personalTitle ctrlSetStructuredText parseText format["Personal Inventory<t align='right'>%1/%2</t>", _carryWeight, _maxWeight];
-
-	private _list = _display displayCtrl 4204;
-	lnbClear _list;
-
-	private _searchValue = toLower (ctrlText (_display displayCtrl 4210));
-
-	{
-		[_list, _searchValue, _x, _y] call _addItem;
 	} forEach ULP_Inventory;
 
 	(_display displayCtrl 4205) ctrlEnable (((lbSize _list) > 0) && { [_container] call ULP_fnc_isCargoUser });
@@ -85,13 +68,39 @@ if (_type isEqualTo 0) then {
 	private _list = _display displayCtrl 4208;
 	lnbClear _list;
 
-	private _searchValue = toLower (ctrlText (_display displayCtrl 4211));
-
 	private _inventory = _container getVariable ["ULP_VirtualCargo", createHashMap];
 	_display setVariable ["lastInventory", _inventory];
 
 	{
-		[_list, _searchValue, _x, _y] call _addItem;
+		private _cfg = missionConfigFile >> "CfgVirtualItems" >> _x;
+		if (isClass _cfg) then {
+			private _itemIcon = getText (_cfg >> "icon");
+
+			if ([getNumber (_cfg >> "Settings" >> "isScripted")] call ULP_fnc_bool) then {
+				{
+					private _row = _list lnbAddRow [format [getText (_cfg >> "displayName"), _x], "-"];
+
+					if !(_itemIcon isEqualTo "") then {
+						_list lnbSetPicture [[_row, 0], _itemIcon];
+					};
+
+					_list lnbSetData [[_row, 0], configName _cfg];
+					_list lnbSetData [[_row, 1], _x];
+				} forEach _y;
+			} else {
+				private _row = _list lnbAddRow [
+					getText (_cfg >> "displayName"), 
+					[_y] call ULP_fnc_numberText
+				];
+
+				if !(_itemIcon isEqualTo "") then {
+					_list lnbSetPicture [[_row, 0], _itemIcon];
+				};
+
+				_list lnbSetData [[_row, 0], _x];
+				_list lnbSetValue [[_row, 1], _y];
+			};
+		};
 	} forEach _inventory;
 
 	(_display displayCtrl 4209) ctrlEnable (((lbSize _list) > 0) && { [_container] call ULP_fnc_isCargoUser });
--- Client Side/Framework/UI/DialogInventory/DialogInventory.hpp
+++ Client Side/Framework/UI/DialogInventory/DialogInventory.hpp
@@ -3,9 +3,6 @@
 #define BODY_HEIGHT 0.400
 #define BODY_Y 0.5 - (BODY_HEIGHT / 2)
 
-#define BODY_CONTENT_Y (BODY_Y + MARGIN_Y)
-#define BODY_CONTENT_LIST_Y (BODY_CONTENT_Y + MARGIN_Y + BUTTON_H)
-
 class DialogInventory {
     idd = 4200;
     movingEnable = 0;
@@ -18,9 +15,9 @@ class DialogInventory {
 			colorBackground[] = HEADER_COLOUR;
 			text = "Container Inventory";
 			SAFEZONE_X(UI_X);
-			SAFEZONE_Y(BODY_Y - BUTTON_H);
+			SAFEZONE_Y(BODY_Y - 0.022);
 			SAFEZONE_W(UI_WIDTH);
-			SAFEZONE_H(BUTTON_H);
+			SAFEZONE_H(0.022);
 		};
 
 		class Body : Life_RscText {
@@ -37,9 +34,9 @@ class DialogInventory {
 			colorBackground[] = INNER_BODY_COLOUR;
 			text = "";
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y);
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BODY_HEIGHT - (MARGIN_Y * 3) - BUTTON_H);
+			SAFEZONE_H(BODY_HEIGHT - (MARGIN_Y * 2));
 		};
 
 		class PersonalHeaderBackground : Life_RscText {
@@ -47,7 +44,7 @@ class DialogInventory {
 			colorBackground[] = { 0, 0, 0, 1 };
 			text = "";
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y);
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
 			SAFEZONE_H(0.044);
 		};
@@ -57,9 +54,9 @@ class DialogInventory {
 			colorBackground[] = INNER_BODY_COLOUR;
 			text = "";
 			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y);
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BODY_HEIGHT - (MARGIN_Y * 3) - BUTTON_H);
+			SAFEZONE_H(BODY_HEIGHT - (MARGIN_Y * 2));
 		};
 
 		class ContainerHeaderBackground : Life_RscText {
@@ -67,7 +64,7 @@ class DialogInventory {
 			colorBackground[] = { 0, 0, 0, 1 };
 			text = "";
 			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y);
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
 			SAFEZONE_H(0.044);
 		};
@@ -84,33 +81,21 @@ class DialogInventory {
 
 	class Controls {
 
-		class PersonalSearch : life_RscEdit {
-			style = "0x10";
-			idc = 4210;
-			text = "";
-			onKeyUp = "[ctrlParent (_this select 0), 0] call ULP_fnc_updateInventory;";
-			colorDisabled[] = {0.95, 0.95, 0.95, 1};
-			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_Y);
-			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BUTTON_H);
-		};
-
 		class Personal : Life_RscStructuredText {
 			idc = 4202;
 			text = "Personal Inventory<t align='right'>0/0</t>";
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y);
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BUTTON_H);
+			SAFEZONE_H(0.022);
 		};
 
 		class PersonalListHeader: Life_RscListNBox {
 			idc = 4203;
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y + BUTTON_H);
+			SAFEZONE_Y((BODY_Y + 0.022) + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BUTTON_H);
+			SAFEZONE_H(0.022);
 			text = "";	
 			rowHeight = 0.04;
 			sizeEx = 0.038;
@@ -136,9 +121,9 @@ class DialogInventory {
 		class PersonalList : Life_RscListNBox {
 			idc = 4204;
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y(BODY_CONTENT_LIST_Y + (BUTTON_H * 2));
+			SAFEZONE_Y((BODY_Y + (0.022 * 2)) + MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H((BODY_HEIGHT - (MARGIN_Y * 3)) - (BUTTON_H * 4));
+			SAFEZONE_H((BODY_HEIGHT - (0.022 * 3))- (MARGIN_Y * 2));
 			text = "";	
 			rowHeight = 0.04;
 			sizeEx = 0.038;
@@ -158,41 +143,76 @@ class DialogInventory {
 			colorBackgroundDisabled[] = {0,0,0,0};
 			onButtonClick = "_this call ULP_fnc_putInCargo;";
 			SAFEZONE_X(UI_X + MARGIN_X);
-			SAFEZONE_Y((BODY_Y + (BODY_HEIGHT - BUTTON_H)) - MARGIN_Y);
+			SAFEZONE_Y((BODY_Y + (BODY_HEIGHT - (0.022))) - MARGIN_Y);
 			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
-			SAFEZONE_H(BUTTON_H);
+			SAFEZONE_H(0.022);
 		};
 
-		#define CONTAINER_X ((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2))
-
-		class ContainerSearch : PersonalSearch {
-			idc = 4211;
-			onKeyUp = "[ctrlParent (_this select 0), 1] call ULP_fnc_updateInventory;";
-			SAFEZONE_X(CONTAINER_X);
-		};
-
-		class Container : Personal {
+		class Container : Life_RscStructuredText {
 			idc = 4206;
 			text = "Container Inventory<t align='right'>0/0</t>";
-			SAFEZONE_X(CONTAINER_X);
+			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
+			SAFEZONE_Y(BODY_Y + MARGIN_Y);
+			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
+			SAFEZONE_H(0.022);
 		};
 
-		class ContainerListHeader: PersonalListHeader {
+		class ContainerListHeader: Life_RscListNBox {
 			idc = 4207;
-			SAFEZONE_X(CONTAINER_X);
+			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
+			SAFEZONE_Y((BODY_Y + 0.022) + MARGIN_Y);
+			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
+			SAFEZONE_H(0.022);
+			text = "";	
+			rowHeight = 0.04;
+			sizeEx = 0.038;
+			columns[] = { 0, 0.6 };
+			drawSideArrows = false;
+			idcLeft = -1;
+			idcRight = -1;
+			disableOverflow = 1;
+
+			class Items {
+				class DisplayName {
+					text = "Item";
+					value = 0;
+				};
+				class Count {
+					text = "Total";
+					value = -1;
+					data = "data";
+				};
+			};
 		};
 
-		class ContainerList : PersonalList {
+		class ContainerList : Life_RscListNBox {
 			idc = 4208;
-			SAFEZONE_X(CONTAINER_X);
+			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
+			SAFEZONE_Y((BODY_Y + (0.022 * 2)) + MARGIN_Y);
+			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
+			SAFEZONE_H((BODY_HEIGHT - (0.022 * 3))- (MARGIN_Y * 2));
+			text = "";	
+			rowHeight = 0.04;
+			sizeEx = 0.038;
+			columns[] = { 0, 0.6 };
+			drawSideArrows = false;
+			idcLeft = -1;
+			idcRight = -1;
 		};
 
-		class ToPersonal : ToInventory {
+		class ToPersonal : ULP_RscButtonIconNoAnim {
 			idc = 4209;
 			text = "\A3\Ui_f\data\GUI\RscCommon\RscHTML\arrow_left_ca.paa";
 			tooltip = "Transfer to self";
+			colorBackground[] = {0,0,0,1};
+            colorFocused[] = {0.09,0.09,0.09,1};
+            colorBackgroundActive[] = {0.03,0.03,0.03,1};
+			colorBackgroundDisabled[] = {0,0,0,0};
 			onButtonClick = "_this call ULP_fnc_takeFromCargo;";
-			SAFEZONE_X(CONTAINER_X);
+			SAFEZONE_X((UI_X + (((UI_WIDTH / 2) - (MARGIN_X / 2)) - MARGIN_X)) + (MARGIN_X * 2));
+			SAFEZONE_Y((BODY_Y + (BODY_HEIGHT - (0.022))) - MARGIN_Y);
+			SAFEZONE_W((((UI_WIDTH - (MARGIN_X * 2)) / 2) - (MARGIN_X / 2)));
+			SAFEZONE_H(0.022);
 		};
 	};
 };

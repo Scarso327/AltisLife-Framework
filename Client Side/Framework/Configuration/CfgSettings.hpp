a4a8f6654768b3457836b55f28fed715ede8e681
Jack Farhall <jack.farhall@rubixx.co.uk>
2025-01-05 16:10:29 +0000

    alter/small-increase-starting-balance

--- Client Side/Framework/Configuration/CfgSettings.hpp
+++ Client Side/Framework/Configuration/CfgSettings.hpp
@@ -8,7 +8,7 @@ class CfgSettings {
     sync_delay = 5 * 60;
     abort_delay = 10;
 
-    starting_bank = 100000;
+    starting_bank = 125000;
     paycheck_period = 15;
 
     buffedSellIncrease = 1.5;

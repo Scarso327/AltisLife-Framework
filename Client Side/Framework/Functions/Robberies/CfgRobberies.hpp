f7c70bca59e604a46ee36683dc1ddaedb4a3c2fb
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-01-06 23:22:14 +0000

    alter: robbery and mission payouts

--- Client Side/Framework/Functions/Robberies/CfgRobberies.hpp
+++ Client Side/Framework/Functions/Robberies/CfgRobberies.hpp
@@ -6,7 +6,7 @@ class CfgRobberies {
 		time = 5 * 60;
 		timeout = 3 * 60;
 		factions[] = { "Civilian" };
-		reward[] = { 30000, 65000 };
+		reward[] = { 50000, 85000 };
 		leveling[] = { { 5, 15 }, "Robbed Fuel Station", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction >= 3";
 		onFail = "[""There must be at least <t color='#B92DE0'>3 officers</t> online to rob this site!""] call ULP_fnc_hint;";
@@ -17,7 +17,7 @@ class CfgRobberies {
 		time = 7 * 60;
 		timeout = 5 * 60;
 		factions[] = { "Civilian" };
-		reward[] = { 50000, 95000 };
+		reward[] = { 80000, 100000 };
 		leveling[] = { { 5, 15 }, "Robbed Supermarket", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction >= 4";
 		onFail = "[""There must be at least <t color='#B92DE0'>4 officers</t> online to rob this site!""] call ULP_fnc_hint;";
@@ -28,7 +28,7 @@ class CfgRobberies {
 		time = 8 * 60;
 		timeout = 15 * 60;
 		factions[] = { "Civilian" };
-		reward[] = { 95000, 220000 };
+		reward[] = { 150000, 250000 };
 		leveling[] = { { 5, 15 }, "Robbed Jewellery Store", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction >= 4";
 		onFail = "[""There must be at least <t color='#B92DE0'>4 officers</t> online to rob this site!""] call ULP_fnc_hint;";
@@ -39,7 +39,7 @@ class CfgRobberies {
 		time = 10 * 60;
 		timeout = 30 * 60;
 		factions[] = { "Civilian" };
-		reward[] = { 140000, 460000 };
+		reward[] = { 200000, 500000 };
 		leveling[] = { { 5, 15 }, "Robbed Fuel Station", 100 };
 		condition = "[[""Police""]] call ULP_fnc_onlineFaction >= 5";
 		onFail = "[""There must be at least <t color='#B92DE0'>5 officers</t> online to rob this site!""] call ULP_fnc_hint;";

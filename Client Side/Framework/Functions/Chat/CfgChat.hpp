d8033033f816ff60ec4a54f83d2a42f37718071e
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-01-24 17:34:08 +0000

    feat: additional chat commands

--- Client Side/Framework/Functions/Chat/CfgChat.hpp
+++ Client Side/Framework/Functions/Chat/CfgChat.hpp
@@ -67,6 +67,15 @@ class CfgChat {
 			action = "[""Total Players:"", format [""%1"", count allPlayers]]";
 			condition = "true";
 		};
+		class Police : Players { action = "[""Total Police:"", format [""%1"", count ([""Police""] call ULP_fnc_allMembers)]]"; };
+		class Medics : Players { action = "[""Total Medics:"", format [""%1"", count ([""Medic""] call ULP_fnc_allMembers)]]"; };
+		class Cops : Police {};
+
+		class Discord : Players { action = """Discord @ discord.gg/mantlenetwork"""; };
+		class Teamspeak : Players { action = """Teamspeak @ ts3.mantlenetwork.co.uk"""; };
+		class TS : Teamspeak {};
+		class TS3 : Teamspeak {};
+
 		class PID {
 			action = "[""ARP ID:"", format [""%1"", [ULP_ID] call ULP_fnc_numberText]]";
 			condition = "(missionNamespace getVariable [""ULP_ID"", -1]) > -1";

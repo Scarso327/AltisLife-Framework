1b67f2495aadece84a0a7d007957067af0d3ab4d
Jack Farhall <jackfarhall@gmail.com>
2025-01-29 23:58:54 +0000

    alter: increment version (#893)

--- Client Side/Framework/Configuration/CfgSettings.hpp
+++ Client Side/Framework/Configuration/CfgSettings.hpp
@@ -1,7 +1,7 @@
 class CfgSettings {
     developers[] = { "76561198092567307", "76561197971322689" };
     
-    framework_version = "1.2.10";
+    framework_version = "1.2.10.1";
 
     total_maxWeight = 5;
 

f887b2b3fc7a1e3b598db390c37f172dfe67afc8
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-02-05 15:25:23 +0000

    feat: salvage yard products

--- Client Side/Framework/Functions/Factories/CfgFactories.hpp
+++ Client Side/Framework/Functions/Factories/CfgFactories.hpp
@@ -2,7 +2,7 @@ class CfgFactories {
 	markerType = "loc_Transmitter";
 	colourDefault = "ColorEast";
 	colourPowered = "ColorIndependent";
-	
+
 	class Altis {
 		class ComponentsFactory {
 			displayName = "Components Factory";
@@ -207,6 +207,320 @@ class CfgFactories {
 						{ "signalJammer", 1 }
 					};
 				};
+			};
+		};
+
+		class SalvageYard {
+			displayName = "Salvage Yard";
+			requiredPower = 1;
+			class Products {
+				class C_Kart_01_black_F {
+					tickTime = 400;
+					materials[] = { 
+						{ "VehicleParts", 1 }
+					};
+				};
+				class C_Kart_01_Blu_F : C_Kart_01_black_F {};
+				class C_Kart_01_Fuel_F : C_Kart_01_black_F {};
+				class C_Kart_01_green_F : C_Kart_01_black_F {};
+				class C_Kart_01_orange_F : C_Kart_01_black_F {};
+				class C_Kart_01_Red_F : C_Kart_01_black_F {};
+				class C_Kart_01_Vrana_F : C_Kart_01_black_F {};
+				class C_Kart_01_white_F : C_Kart_01_black_F {};
+				class C_Kart_01_yellow_F : C_Kart_01_black_F {};
+				class C_Rubberboat : C_Kart_01_black_F {};
+				class C_Quadbike_01_F : C_Kart_01_black_F {};
+				class C_Scooter_Transport_01_F : C_Kart_01_black_F {};
+
+				class C_Hatchback_01_F {
+					tickTime = 425;
+					materials[] = { 
+						{ "VehicleParts", 1 },
+						{ "Tyre", 1 }
+					};
+				};
+				class C_Tractor_01_F : C_Hatchback_01_F {};
+				class C_Offroad_02_unarmed_F : C_Hatchback_01_F {
+					materials[] = { 
+						{ "VehicleParts", 2 },
+						{ "Tyre", 1 }
+					};
+				};
+				class C_IDAP_Offroad_02_unarmed_F : C_Offroad_02_unarmed_F {};
+				class B_GEN_Offroad_01_gen_F : C_Offroad_02_unarmed_F {};
+				class C_Offroad_01_F : C_Offroad_02_unarmed_F {};
+				class C_IDAP_Offroad_01_F : C_Offroad_02_unarmed_F {};
+				class C_Offroad_01_covered_F : C_Offroad_02_unarmed_F {};
+				class C_Offroad_01_comms_F : C_Offroad_02_unarmed_F {};
+				class C_Offroad_01_repair_F : C_Offroad_02_unarmed_F {};
+
+				class C_SUV_01_F {
+					tickTime = 450;
+					materials[] = { 
+						{ "VehicleParts", 2 },
+						{ "Tyre", 1 },
+						{ "GlassPanel", 1 }
+					};
+				};
+				class C_Van_02_medevac_F : C_SUV_01_F {
+					materials[] = { 
+						{ "VehicleParts", 3 },
+						{ "Tyre", 2 },
+						{ "GlassPanel", 1 },
+						{ "SmallCargoBed", 1 }
+					};
+				};
+				class B_GEN_Van_02_transport_F : C_Van_02_medevac_F {};
+				class C_Van_02_transport_F : C_Van_02_medevac_F {};
+				class C_IDAP_Van_02_transport_F : C_Van_02_medevac_F {};
+				class C_Van_02_vehicle_F : C_Van_02_medevac_F {};
+				class C_Van_02_service_F : C_Van_02_medevac_F {};
+				class C_IDAP_Van_02_vehicle_F : C_Van_02_medevac_F {};
+				class C_Van_01_transport_F : C_Van_02_medevac_F {};
+				class B_UAV_01_F : C_SUV_01_F {
+					requiredPower = 2;
+					materials[] = { 
+						{ "VehicleParts", 3 }
+					};
+				};
+				class C_UAV_06_medical_F : B_UAV_01_F {};
+
+				class C_Van_01_box_F {
+					tickTime = 475;
+					materials[] = { 
+						{ "VehicleParts", 4 },
+						{ "Tyre", 2 },
+						{ "GlassPanel", 1 },
+						{ "SmallCargoBed", 1 }
+					};
+				};
+				class C_Van_01_fuel_F : C_Van_01_box_F {};
+				class C_Boat_Civil_01_F : C_Van_01_box_F {
+					materials[] = { 
+						{ "VehicleParts", 4 }
+					};
+				};
+				class C_Boat_Civil_01_police_F : C_Boat_Civil_01_F {};
+				class C_Hatchback_01_sport_F : C_Van_01_box_F {
+					materials[] = { 
+						{ "VehicleParts", 4 },
+						{ "GlassPanel", 1 },
+						{ "EngineComponent", 1 }
+					};
+				};
+				class C_Boat_Transport_02_F : C_Van_01_box_F {
+					materials[] = { 
+						{ "VehicleParts", 4 },
+						{ "GuidanceSystem", 1 }
+					};
+				};
+				class B_SDV_01_F : C_Boat_Transport_02_F {
+					requiredPower = 2;
+				};
+
+				class I_Truck_02_transport_F {
+					tickTime = 500;
+					materials[] = { 
+						{ "VehicleParts", 5 },
+						{ "Tyre", 41 },
+						{ "GlassPanel", 2 },
+						{ "SmallCargoBed", 1 }
+					};
+				};
+				class C_IDAP_Truck_02_transport_F : I_Truck_02_transport_F {};
+				class C_IDAP_Truck_02_water_F : I_Truck_02_transport_F {};
+				class C_IDAP_Truck_02_F : I_Truck_02_transport_F {};
+				class C_Heli_Light_01_civil_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 6 },
+						{ "BasicFlightInstruments", 2 }
+					};
+				};
+				class B_Truck_01_mover_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 6 },
+						{ "ReinforcedTyre", 1 },
+						{ "LightArmourPlate", 1 }
+					};
+				};
+				class B_Truck_01_flatbed_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 6 },
+						{ "ReinforcedTyre", 1 },
+						{ "LightArmourPlate", 1 },
+						{ "LargeCargoBed", 1 }
+					};
+				};
+				class C_Heli_Light_01_civil_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 6 },
+						{ "BasicFlightInstruments", 2 },
+						{ "GuidanceSystem", 1 }
+					};
+				};
+				class B_CTRG_LSV_01_light_F : I_Truck_02_transport_F {
+					materials[] = { 
+						requiredPower = 2;
+						{ "VehicleParts", 6 },
+						{ "LightArmourPlate", 1 },
+						{ "ReinforcedTyre", 1 }
+					};
+				};
+				class B_LSV_01_unarmed_black_F : B_CTRG_LSV_01_light_F {};
+				class I_C_Offroad_02_LMG_F : B_CTRG_LSV_01_light_F {};
+				class O_LSV_02_unarmed_F : B_CTRG_LSV_01_light_F {};
+				class I_Truck_02_covered_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 5 },
+						{ "Tyre", 4 },
+						{ "GlassPanel", 2 },
+						{ "SmallCargoBed", 1 }
+					};
+				};
+				class I_Truck_02_fuel_F : I_Truck_02_covered_F {};
+				class O_Truck_03_transport_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 8 },
+						{ "LargeCargoBed", 1 },
+						{ "HeavyArmourPlate", 1 },
+						{ "ReinforcedTyre", 1 },
+						{ "EngineComponent", 1 }
+					};
+				};
+				class O_Truck_03_covered_F : O_Truck_03_transport_F {};
+				class O_Truck_03_ammo_F : O_Truck_03_transport_F {};
+				class O_Truck_03_repair_F : O_Truck_03_transport_F {};
+				class O_Truck_03_fuel_F : O_Truck_03_transport_F {};
+				class O_Truck_03_device_F : O_Truck_03_transport_F {
+					requiredPower = 3;
+				};
+				class B_Truck_01_cargo_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 8 },
+						{ "LargeCargoBed", 1 },
+						{ "HeavyArmourPlate", 1 }
+					};
+				};
+				class B_Truck_01_transport_F : B_Truck_01_cargo_F {};
+				class B_Truck_01_covered_F : B_Truck_01_cargo_F {};
+				class B_Truck_01_ammo_F : B_Truck_01_cargo_F {};
+				class B_Truck_01_fuel_F : B_Truck_01_cargo_F {};
+				class B_Truck_01_box_F : B_Truck_01_cargo_F {
+					requiredPower = 2;
+					materials[] = { 
+						{ "VehicleParts", 8 },
+						{ "LargeCargoBed", 1 },
+						{ "HeavyArmourPlate", 1 },
+						{ "EngineComponent", 1 },
+						{ "ImprovedVehicleComponent", 1 }
+					};
+				};
+				class C_Plane_Civil_01_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 }
+					};
+				};
+				class C_Plane_Civil_01_racing_F : C_Plane_Civil_01_F {};
+				class O_Heli_Light_02_unarmed_F : I_Truck_02_transport_F {
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 },
+						{ "BasicFlightInstruments", 1 }
+					};
+				};
+				class I_Heli_light_03_unarmed_F : I_Truck_02_transport_F {
+					requiredPower = 2;
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 },
+						{ "BasicFlightInstruments", 1 },
+						{ "ImprovedVehicleComponent", 1 }
+					};
+				};
+
+				class B_G_Offroad_01_armed_F {
+					tickTime = 550;
+					requiredPower = 2;
+					materials[] = { 
+						{ "VehicleParts", 8 },
+						{ "ReinforcedTyre", 2 },
+						{ "HeavyArmourPlate", 1 },
+						{ "EngineComponent", 1 },
+						{ "ImprovedVehicleComponent", 1 }
+					};
+				};
+				class I_MRAP_03_F : B_G_Offroad_01_armed_F {
+					requiredPower = 3;
+				};
+				class O_MRAP_02_F : I_MRAP_03_F {};
+				class B_MRAP_01_F : I_MRAP_03_F {};
+				class O_LSV_02_armed_black_F : I_MRAP_03_F {};
+
+				class O_Heli_Transport_04_bench_black_F {
+					tickTime = 600;
+					requiredPower = 2;
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 },
+						{ "AdvancedFlightInstruments", 1 },
+						{ "ImprovedVehicleComponent", 1 }
+					};
+				};
+				class O_Heli_Transport_04_black_F : O_Heli_Transport_04_bench_black_F {};
+				class O_Heli_Transport_04_medevac_black_F : O_Heli_Transport_04_bench_black_F {};
+				class O_Heli_Transport_04_covered_black_F : O_Heli_Transport_04_bench_black_F {};
+				class O_Heli_Transport_04_fuel_black_F : O_Heli_Transport_04_bench_black_F {};
+				class O_Heli_Transport_04_box_black_F : O_Heli_Transport_04_bench_black_F {};
+				class O_Heli_Transport_04_ammo_black_F : O_Heli_Transport_04_bench_black_F {};
+				class I_Heli_Transport_02_F : O_Heli_Transport_04_bench_black_F {};
+				class C_IDAP_Heli_Transport_02_F : O_Heli_Transport_04_bench_black_F {};
+				class B_Boat_Armed_01_minigun_F : O_Heli_Transport_04_bench_black_F {
+					requiredPower = 3;
+					materials[] = { 
+						{ "VehicleParts", 10 }
+					};
+				};
+				class B_Heli_Transport_01_F : B_Boat_Armed_01_minigun_F {
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 },
+						{ "AdvancedFlightInstruments", 1 },
+						{ "PerfectedVehicleComponent", 1 }
+					};
+				};
+				class I_Heli_light_03_F : B_Boat_Armed_01_minigun_F {};
+
+				class B_Heli_Transport_03_unarmed_F {
+					tickTime = 700;
+					requiredPower = 3;
+					materials[] = { 
+						{ "VehicleParts", 9 },
+						{ "GuidanceSystem", 1 },
+						{ "EngineComponent", 1 },
+						{ "AdvancedFlightInstruments", 1 },
+						{ "PerfectedVehicleComponent", 1 }
+					};
+				};
+				class I_Plane_Fighter_03_CAS_F : B_Heli_Transport_03_unarmed_F {};
+				class I_Plane_Fighter_04_F : B_Heli_Transport_03_unarmed_F {};
+				class B_T_VTOL_01_infantry_F : B_Heli_Transport_03_unarmed_F {};
+				class B_T_VTOL_01_vehicle_F : B_Heli_Transport_03_unarmed_F {};
+				class O_Plane_CAS_02_F : B_Heli_Transport_03_unarmed_F {};
+				class B_Plane_CAS_01_F : B_Heli_Transport_03_unarmed_F {};
+				class B_Plane_Fighter_01_F : B_Heli_Transport_03_unarmed_F {};
+				class O_Plane_Fighter_02_F : B_Heli_Transport_03_unarmed_F {};
+				class B_Plane_Fighter_01_Stealth_F : B_Heli_Transport_03_unarmed_F {};
+				class O_T_VTOL_02_infantry_F : B_Heli_Transport_03_unarmed_F {};
+				class O_T_VTOL_02_vehicle_F : B_Heli_Transport_03_unarmed_F {};
+				class O_Plane_Fighter_02_Stealth_F : B_Heli_Transport_03_unarmed_F {};
+				class B_Heli_Attack_01_F : B_Heli_Transport_03_unarmed_F {};
+				class O_Heli_Attack_02_F : B_Heli_Transport_03_unarmed_F {};
 			};
 		};
 	};

07399afc305681237c08140bc0e4d9178ec29186
Jack Farhall <jack.farhall@rubixx.co.uk>
2025-02-06 19:40:46 +0000

    feat: greenzone rebel outpost for gang wars

--- Client Side/Framework/Functions/Bases/CfgBases.hpp
+++ Client Side/Framework/Functions/Bases/CfgBases.hpp
@@ -5,6 +5,7 @@ class CfgBases {
 	RedzoneKillReward = 15000;
 	BidIncrement = 1000000;
 	StartingBid = 9000000; // StartingBid = BidIncrement + StartingBid
+	GangWarsOnlyMarkers[] = { "rebel_outpost_3", "greenzone_06" };
 
 	class North {
 		icon = "Data\UI\Spawns\base.paa";

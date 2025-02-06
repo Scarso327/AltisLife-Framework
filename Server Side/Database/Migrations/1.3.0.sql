2eb8642a6d3fda0c3ec63419f8912c4db53366ae
Jack Farhall <jackfarhall@gmail.com>
2025-02-06 23:24:40 +0000

    alter: sql to update everyone with 125k and 0 cash to new 150k (#989)

--- Server Side/Database/Migrations/1.3.0.sql
+++ Server Side/Database/Migrations/1.3.0.sql
@@ -2,3 +2,5 @@ ALTER TABLE `vehicles`
 	ADD COLUMN `upgrades` TEXT NOT NULL AFTER `texture`;
 
 UPDATE `vehicles` SET `upgrades` = '"[]"';
+
+UPDATE `players` SET `bankacc` = 150000  WHERE `bankacc` = 125000 AND `cash` = 0;

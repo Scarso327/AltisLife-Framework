d588e6e7402179c23da307823f08081eb56a0a58
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-02-06 18:53:21 +0000

    alter: action text for shovelling and extracting

--- Client Side/Framework/Functions/Gathering/CfgGathering.hpp
+++ Client Side/Framework/Functions/Gathering/CfgGathering.hpp
@@ -93,12 +93,14 @@ class CfgGathering {
 		leveling[] = { 3, "Gathering", 100 };
 	};
 	class Oil : BaseMine {
+		actionTitle = "Extracting";
 		reqItems[] = { "Extractor" };
 		areas[] = { { "oil_extraction", 20 } };
 		items[] = { { "UnprocessedOil", 100 } };
 		leveling[] = { 5, "Gathering", 100 };
 	};
 	class Sand : Oil {
+		actionTitle = "Shovelling";
 		reqItems[] = { "Shovel" };
 		areas[] = { { "sand_mine", 20 } };
 		items[] = { { "Sand", 100 } };
@@ -118,7 +120,6 @@ class CfgGathering {
 		leveling[] = { 5, "Gathering", 100 };
 	};
 	class Uranium : Coral {
-		hidden[] = {};
 		areas[] = { { "uranium_mine", 20 } };
 		items[] = { { "UraniumOre", 100 } };
 		leveling[] = { 10, "Gathering", 70 };

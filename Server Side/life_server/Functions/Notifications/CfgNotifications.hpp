f943f2ba3eb0c8842ce508473e99c26d35b88ac0
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-03-09 13:40:11 +0000

    alter: icons and colour for server feed

--- Server Side/life_server/Functions/Notifications/CfgNotifications.hpp
+++ Server Side/life_server/Functions/Notifications/CfgNotifications.hpp
@@ -2,14 +2,18 @@ class CfgNotifications {
 	class PopupCartel {
 		displayName = "Popup Cartel";
 		message = "A %1 has spawned %2";
-		colour = "00FF00";
-		image = "https://mantlenetwork.co.uk/_next/image?url=%2Flogos%2Fapple-icon-180.png&w=96&q=75";
+		colour = "990000";
+		image = "https://media.discordapp.net/attachments/760596158656807003/1348241166382141450/popup.webp?ex=67cebeef&is=67cd6d6f&hm=05c7ac2dcf5d6cdcca7337c9bce908fd6ef095ff07556c8f8928a1190259454e&=&format=webp&width=168&height=168";
 	};
 	class Shipwreck : PopupCartel {
 		displayName = "Shipwreck";
+		colour = "1155CC";
+		image = "https://media.discordapp.net/attachments/760596158656807003/1348241156936302693/shipwreck.webp?ex=67cebeec&is=67cd6d6c&hm=59a1a1389b06693fb8295eb64457513e8d107c30994cc90968d00cfa2c12d3d9&=&format=webp&width=168&height=168";
 	};
 	class Airdrop : PopupCartel {
 		displayName = "Airdrop";
 		message = "An %1 has spawned %2";
+		colour = "BF9000";
+		image = "https://media.discordapp.net/attachments/760596158656807003/1348241190763499600/airdrop.webp?ex=67cebef4&is=67cd6d74&hm=5a542423ec1ca7cee4b88e7d86f2e8907d2e6398d35b2a4340bd8293bb64a775&=&format=webp&width=168&height=168";
 	};
 };

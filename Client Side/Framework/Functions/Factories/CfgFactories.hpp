87550f5e42943a1dd928aa507de8b14c129fee29
Lewis <LewisAllanMackinnon@Gmail.Com>
2025-03-25 02:00:06 +0000

    alter: current factory material requirements

--- Client Side/Framework/Functions/Factories/CfgFactories.hpp
+++ Client Side/Framework/Functions/Factories/CfgFactories.hpp
@@ -19,73 +19,67 @@ class CfgFactories {
 					tickTime = 5;
 					materials[] = { 
 						{ "RubberSheet", 2 },
-						{ "SteelPlate", 1 }
+						{ "IronBar", 2 }
 					};
 				};
 				class ReinforcedTyre {
 					requiredPower = 2;
 					tickTime = 10;
 					materials[] = { 
-						{ "RubberSheet", 2 },
-						{ "SteelPlate", 2 },
-						{ "FlawlessDiamond", 1 }
+						{ "Tyre", 4 },
+						{ "SteelPlate", 1 }
 					};
 				};
 				class LightArmourPlate {
 					tickTime = 15;
-					materials[] = { 
-						{ "SteelPlate", 8 }
-					};
+					materials[] = { { "IronPlate", 2 } };
 				};
 				class HeavyArmourPlate {
 					requiredPower = 2;
 					tickTime = 20;
 					materials[] = { 
-						{ "SteelPlate", 16 },
-						{ "FlawlessDiamond", 4 }
+						{ "LightArmourPlate", 1 },
+						{ "FlawlessDiamond", 1 },
+						{ "SteelPlate", 2 },
 					};
 				};
 				class SmallCargoBed {
 					tickTime = 10;
 					materials[] = { 
 						{ "Plank", 8 },
-						{ "SteelPlate", 4 }
+						{ "IronPlate", 2 }
 					};
 				};
 				class LargeCargoBed {
 					requiredPower = 2;
 					tickTime = 20;
 					materials[] = { 
-						{ "Plank", 24 },
-						{ "SteelPlate", 6 }
+						{ "SmallCargoBed", 2 },
+						{ "SteelPlate", 1 }
 					};
 				};
 				class GuidanceSystem {
 					tickTime = 20;
 					materials[] = { 
-						{ "SilverPlate", 4 },
-						{ "FlawlessDiamond", 3 },
-						{ "CopperPlate", 2 },
-						{ "CopperWiring", 8 }
+						{ "SilverPlate", 1 },
+						{ "FlawlessDiamond", 4 },
+						{ "ElectronicCircuit", 2 }
 					};
 				};
 				class EngineComponent {
 					requiredPower = 2;
 					tickTime = 25;
 					materials[] = { 
-						{ "SteelPlate", 12 },
-						{ "CopperPlate", 4 },
-						{ "PackedOil", 3 }
+						{ "SteelPlate", 2 },
+						{ "CopperPlate", 1 },
+						{ "PackedOil", 4 }
 					};
 				};
 				class BasicFlightInstruments {
 					tickTime = 30;
 					materials[] = { 
-						{ "CopperWiring", 16 },
-						{ "SilverPlate", 3 },
+						{ "CentralProcessingUnit", 8 },
 						{ "GlassPanel", 2 },
-						{ "SteelPlate", 2 },
-						{ "FlawlessDiamond", 1 },
 						{ "GuidanceSystem", 1 }
 					};
 				};
@@ -93,43 +87,37 @@ class CfgFactories {
 					requiredPower = 2;
 					tickTime = 60;
 					materials[] = { 
-						{ "CopperWiring", 32 },
-						{ "FlawlessDiamond", 4 },
-						{ "SilverPlate", 4 },
+						{ "BasicFlightInstruments", 1 },
+						{ "FlawlessDiamond", 6 },
+						{ "SilverPlate", 2 },
 						{ "GlassPanel", 4 },
-						{ "SteelPlate", 3 },
-						{ "GuidanceSystem", 3 },
-						{ "ReinforcedGlassPanel", 2 }
+						{ "GuidanceSystem", 1 }
 					};
 				};
 				class SimpleVehicleComponent {
-					tickTime = 15;
+					tickTime = 10;
 					materials[] = { 
 						{ "VehicleParts", 4 },
-						{ "SteelPlate", 4 }
+						{ "CopperPlate", 4 }
 					};
 				};
 				class ImprovedVehicleComponent {
 					requiredPower = 2;
-					tickTime = 45;
+					tickTime = 20;
 					materials[] = { 
-						{ "SteelPlate", 6 },
 						{ "SimpleVehicleComponent", 4 },
-						{ "FlawlessDiamond", 3 },
-						{ "SilverPlate", 2 },
-						{ "CopperPlate", 2 }
+						{ "FlawlessDiamond", 2 },
+						{ "SilverPlate", 2 }
 					};
 				};
 				class PerfectedVehicleComponent {
 					requiredPower = 3;
-					tickTime = 90;
+					tickTime = 30;
 					materials[] = { 
-						{ "SteelPlate", 8 },
-						{ "FlawlessDiamond", 6 },
-						{ "SimpleVehicleComponent", 4 },
+						{ "ImprovedVehicleComponent", 4 },
+						{ "FlawlessDiamond", 8 },
 						{ "PackedOil", 4 },
-						{ "SilverPlate", 3 },
-						{ "CopperPlate", 3 }
+						{ "SteelPlate", 2 }
 					};
 				};
 
@@ -139,138 +127,83 @@ class CfgFactories {
 					tickTime = 120;
 					materials[] = { 
 						{ "SteelPlate", 4 },
-						{ "PackedOil", 2 },
+						{ "PackedOil", 4 },
 						{ "LargeCargoBed", 1 },
-						{ "EngineComponent", 1 },
-						{ "ReinforcedTyre", 1 }
+						{ "EngineComponent", 1 }
 					};
 				};
 				class ImprovedLocksUpgrade : ImprovedStorageUpgrade {
+					requiredPower = 1;
 					materials[] = { 
-						{ "SilverPlate", 4 },
-						{ "ReinforcedGlassPanel", 4 },
-						{ "LightArmourPlate", 2 }
+						{ "SilverPlate", 2 },
+						{ "LightArmourPlate", 2 },
+						{ "FlawlessDiamond", 1 }
 					};
 				};
 				class ImprovedHullUpgrade : ImprovedStorageUpgrade {
-					materials[] = { 
-						{ "ReinforcedGlassPanel", 4 },
-						{ "HeavyArmourPlate", 2 }
-					};
+					requiredPower = 1;
+					materials[] = { { "HeavyArmourPlate", 2 } };
 				};
 				class ImprovedTyresUpgrade : ImprovedStorageUpgrade {
-					materials[] = { 
-						{ "ReinforcedTyre", 4 },
-						{ "SteelPlate", 4 }
-					};
+					requiredPower = 1;
+					materials[] = { { "ReinforcedTyre", 4 } };
 				};
-				// class PerformanceEngineUpgrade : ImprovedStorageUpgrade {
-				// 	materials[] = { 
-				// 		{ "CopperWiring", 4 },
-				// 		{ "FlawlessDiamond", 2 },
-				// 		{ "EngineComponent", 2 },
-				// 		{ "PackedOil", 2 }
-				// 	};
-				// };
 				class EfficientEngineUpgrade : ImprovedStorageUpgrade {
+					requiredPower = 1;
 					materials[] = { 
 						{ "PackedOil", 4 },
-						{ "SteelPlate", 4 },
-						{ "EngineComponent", 2 }
+						{ "EngineComponent", 1 }
 					};
 				};
 				class StongCasingUpgrade : ImprovedStorageUpgrade {
 					materials[] = { 
-						{ "SteelPlate", 4 },
-						{ "HeavyArmourPlate", 4 },
-						{ "FlawlessDiamond", 2 }
+						{ "ReinforcedGlassPanel", 4 },
+						{ "HeavyArmourPlate", 4 }
 					};
 				};
 				class TrackingDeviceUpgrade : ImprovedStorageUpgrade {
-					materials[] = { 
-						{ "CopperWiring", 5 },
-						{ "SilverBar", 4 },
-						{ "GuidanceSystem", 2 }
-					};
+					requiredPower = 1;
+					materials[] = { { "GuidanceSystem", 1 } };
 				};
 				class ClonedPlatesUpgrade : ImprovedStorageUpgrade {
+					requiredPower = 1;
 					materials[] = { 
-						{ "CopperWiring", 5 },
-						{ "CopperPlate", 2 },
+						{ "CopperPlate", 1 },
 						{ "SignalJammer", 1 }
 					};
 				};
 
 				// Housing Upgrades
 				class PersonalAlarmUpgrade {
-					requiredPower = 2;
+					requiredPower = 1;
 					tickTime = 120;
 					materials[] = { 
-						{ "ElectronicCircuit", 3 },
-						{ "FlawlessDiamond", 2 },
-						{ "CopperWiring", 2 },
+						{ "ElectronicCircuit", 2 },
 						{ "ReinforcedGlassPanel", 1 },
 						{ "Laptop", 1 }
 					};
 				};
 				class CentralAlarmUpgrade : PersonalAlarmUpgrade {
+					requiredPower = 2;
 					materials[] = { 
-						{ "ElectronicCircuit", 3 },
-						{ "FlawlessDiamond", 3 },
-						{ "CopperWiring", 2 },
-						{ "ReinforcedGlassPanel", 1 },
-						{ "Laptop", 1 },
+						{ "PersonalAlarmUpgrade", 1 },
+						{ "FlawlessDiamond", 2 },
 						{ "PanicButton", 1 }
 					};
 				};
 				class ImprovedDoorsUpgrade : PersonalAlarmUpgrade {
-					materials[] = { 
-						{ "SilverPlate", 8 },
-						{ "RubberSheet", 8 },
-						{ "HeavyArmourPlate", 4 }
-					};
+					materials[] = { { "HeavyArmourPlate", 2 } };
 				};
-				// class BulletproofWindowsUpgrade : PersonalAlarmUpgrade {
-				// 	materials[] = { 
-				// 		{ "ReinforcedGlassPanel", 12 },
-				// 		{ "HeavyArmourPlate", 2 }
-				// 	};
-				// };
 				class ImprovedContainerUpgrade : PersonalAlarmUpgrade {
+					requiredPower = 2;
 					materials[] = { 
-						{ "SteelPlate", 4 },
-						{ "LargeCargoBed", 4 },
-						{ "SmallCargoBed", 2 }
+						{ "SteelPlate", 6 },
+						{ "LargeCargoBed", 4 }
 					};
 				};
-				// class SingalJammerUpgrade : PersonalAlarmUpgrade {
-				// 	materials[] = { 
-				// 		{ "ElectronicCircuit", 4 },
-				// 		{ "CopperWiring", 2 },
-				// 		{ "FlawlessDiamond", 1 },
-				// 		{ "ReinforcedGlassPanel", 1 },
-				// 		{ "Laptop", 1 },
-				// 		{ "SignalJammer", 1 }
-				// 	};
-				// };
-				// class PersonalFridgeUpgrade : PersonalAlarmUpgrade {
-				// 	materials[] = { 
-				// 		{ "CopperPlate", 2 },
-				// 		{ "GlassPanel", 2 },
-				// 		{ "RubberSheet", 1 }
-				// 	};
-				// };
-				// class PersonalWorkbenchUpgade : PersonalAlarmUpgrade {
-				// 	materials[] = { 
-				// 		{ "Toolbox", 4 },
-				// 		{ "IronPlate", 4 },
-				// 		{ "HeavyArmourPlate", 1 }
-				// 	};
-				// };
 				class PersonalMailboxUpgrade : PersonalAlarmUpgrade {
 					materials[] = { 
 						{ "FlawlessDiamond", 4 },
-						{ "PrintedCircuitBoard", 2 },
 						{ "Desktop", 1 }
 					};
 				};

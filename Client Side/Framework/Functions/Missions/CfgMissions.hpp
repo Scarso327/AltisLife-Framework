040b69e6f1c3d5bacef48cf695ad28c1355fe950
Lewis Mackinnon <LewisAllanMackinnon@Gmail.Com>
2025-04-12 12:47:40 +0100

    fix: veh requirement for humanitarian missions

--- Client Side/Framework/Functions/Missions/CfgMissions.hpp
+++ Client Side/Framework/Functions/Missions/CfgMissions.hpp
@@ -668,10 +668,7 @@ class CfgMissions {
 		name = "Humanitarian Mission";
 		description = "You need to deliver supplies to %1";
 		condition = "[""IDAP""] call ULP_fnc_hasLicense";
-		vehicleRequirement[] = { 
-			{ "Car" }, 10,
-			{ "Helicopter", 25 }
-		};
+		vehicleRequirement[] = { { "Car", "Helicopter" }, 10, };
 		cargoItem = "HumanitarianSupplies";
 		class Locations {
 			class IDAP_HQ {
